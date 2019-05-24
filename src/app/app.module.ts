f788ec0218849428207dd61535fc59a3d6c75311
emilio <emiliocortina@gmail.com>
2019-05-24 20:07:36 +0200

    Trying to integrate warmup in Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Task2Component } from './task2/task2.component';
+import { Task1Component } from './task1/task1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Task2Component,
+    Task1Component
   ],
   imports: [
     BrowserModule
