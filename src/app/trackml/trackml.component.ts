f0e710092db2a1940f5670579db5422c19c92a64
emilio <emiliocortina@gmail.com>
2019-06-04 17:55:25 +0200

    Adding components and refactoring ThreeJS

---
+++ src/app/trackml/trackml.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trackml',
+  templateUrl: './trackml.component.html',
+  styleUrls: ['./trackml.component.css']
+})
+export class TrackmlComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
