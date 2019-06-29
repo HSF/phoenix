0e3228b27c74d8a22bb2f3c5544a7feeb7b130ef
emilio <emiliocortina@gmail.com>
2019-06-29 14:36:21 +0200

    Adding options to Playground VR

--- src/app/playground-vr/playground-vr.component.ts
+++ src/app/playground-vr/playground-vr.component.ts
@@ -15,4 +15,33 @@ export class PlaygroundVrComponent implements OnInit {
     this.eventDisplay.initVR(new Configuration());
   }
 
+  handleFileInput(files: any) {
+    const file = files[0];
+    const reader = new FileReader();
+    if (file.type === 'application/json') {
+      reader.onload = () => {
+        const json = JSON.parse(reader.result.toString());
+        this.eventDisplay.buildEventDataFromJSON(json);
+      };
+      reader.readAsText(file);
+    }
+    if (file.name.split('.').pop() === 'obj') {
+      reader.onload = () => {
+        this.eventDisplay.loadGeometryFromOBJContent(reader.result.toString(), file.name.split('.')[0]);
+      };
+      reader.readAsText(file);
+    }
+    if (file.name.split('.').pop() === 'gltf') {
+      reader.onload = () => {
+        this.eventDisplay.loadDisplay(reader.result.toString());
+      };
+      reader.readAsText(file);
+    } else {
+      console.log('Error : ¡ Invalid file format !');
+    }
+  }
+
+  saveConfiguration() {
+    this.eventDisplay.saveDisplay();
+  }
 }
