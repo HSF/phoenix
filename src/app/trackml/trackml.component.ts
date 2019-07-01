754a6a57d0e77e4acf5fe5ec317cbe4f8634a164
emilio <emiliocortina@gmail.com>
2019-07-01 14:12:28 +0200

    Adding preset views to ATLAS and TtrackML

--- src/app/trackml/trackml.component.ts
+++ src/app/trackml/trackml.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {EventdisplayService} from '../services/eventdisplay.service';
 import {Configuration} from '../services/configuration';
+import {PresetView} from '../services/preset-view';
 
 @Component({
   selector: 'app-trackml',
@@ -13,7 +14,13 @@ export class TrackmlComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.eventDisplay.init(new Configuration());
+    const configuration = new Configuration();
+    configuration.presetViews = [
+      new PresetView('Right View', [0, 0, 6000], 'right.svg'),
+      new PresetView('Center View', [-500, 1000, 0], 'circle.svg'),
+      new PresetView('Left View', [0, 0, -6000], 'left.svg')
+    ];
+    this.eventDisplay.init(configuration);
     this.eventDisplay.loadGeometryFromOBJ('assets/geometry/TrackML/strip_long_simplified.obj', 'Long Strip', 0xe9a23b, true);
     this.eventDisplay.loadGeometryFromOBJ('assets/geometry/TrackML/pixel_simplified.obj', 'Pixel', 0xe2a9e8, true);
     this.eventDisplay.loadGeometryFromOBJ('assets/geometry/TrackML/strip_short_simplified.obj', 'Short Strip', 0x369f95, true);
