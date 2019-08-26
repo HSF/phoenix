5c2d75763591f83ee1156ea2cf823a7b892931d1
Emilio Cortina Labra <emiliocortina@gmail.com>
2019-08-26 16:20:07 -0400

    Update trackml.component.spec.ts

--- src/app/sections/trackml/trackml.component.spec.ts
+++ src/app/sections/trackml/trackml.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 import { TrackmlComponent } from './trackml.component';
 
 describe('TrackmlComponent', () => {
