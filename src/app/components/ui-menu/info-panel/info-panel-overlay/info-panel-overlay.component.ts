1c90658449e8d98da749baa6bd5df57d7cacdc8c
ginpachi <m.fawaadali98@gmail.com>
2020-05-10 17:28:48 +0500

    Add complete docs

--- src/app/components/ui-menu/info-panel/info-panel-overlay/info-panel-overlay.component.ts
+++ src/app/components/ui-menu/info-panel/info-panel-overlay/info-panel-overlay.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { InfoLoggerService } from '../../../../services/infologger.service';
 
+/**
+ * Component for displaying information from the logger
+ */
 @Component({
   selector: 'app-info-panel-overlay',
   templateUrl: './info-panel-overlay.component.html',
@@ -8,11 +11,20 @@ import { InfoLoggerService } from '../../../../services/infologger.service';
 })
 export class InfoPanelOverlayComponent implements OnInit {
 
+  /** @ignore */
   @Input() showInfoPanel: boolean;
+  /** List of actions to be displayed in the info panel */
   actionsList = [];
 
+  /**
+   * Create the information panel overlay
+   * @param infoLogger Service containing operations related to info logs
+   */
   constructor(private infoLogger: InfoLoggerService) { }
 
+  /**
+   * Get the list of logs from the info logger service
+   */
   ngOnInit() {
     this.actionsList = this.infoLogger.getInfoLoggerList();
   }
