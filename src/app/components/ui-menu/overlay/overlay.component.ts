055c2831f7f091a04a7ff6194406161cec7a813a
ginpachi <m.fawaadali98@gmail.com>
2020-05-12 04:22:54 +0500

    Add resizable option for overlays

--- src/app/components/ui-menu/overlay/overlay.component.ts
+++ src/app/components/ui-menu/overlay/overlay.component.ts
@@ -1,15 +1,61 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-overlay',
   templateUrl: './overlay.component.html',
   styleUrls: ['./overlay.component.scss']
 })
-export class OverlayComponent {
+export class OverlayComponent implements AfterViewInit {
 
   @Input() title: string;
   @Input() active: boolean = false;
   @Input() icon: string;
+  @Input() resizable: boolean = false;
   showBody: boolean = true;
+
+  @ViewChild('overlayCard') overlayCard: ElementRef;
+  @ViewChild('resizeHandleCorner') resizeHandleCorner: ElementRef;
+
+  ngAfterViewInit() {
+    if (this.resizable) {
+      const resizeHandleElement = this.resizeHandleCorner.nativeElement;
+      resizeHandleElement.style.bottom = '0';
+      resizeHandleElement.style.right = '0';
+    }
+  }
+
+  dragMove() {
+    const resizeHandleElement = this.resizeHandleCorner.nativeElement;
+    const overlayCardElement = this.overlayCard.nativeElement;
+
+    const dragRect = resizeHandleElement.getBoundingClientRect();
+    const targetRect = overlayCardElement.getBoundingClientRect();
+
+    const width = dragRect.left - targetRect.left + dragRect.width;
+    const height = dragRect.top - targetRect.top + dragRect.height;
+
+    if ((width > 300 && height > 100)
+      && (width < window.innerWidth / 2 && height < window.innerHeight / 1.2)) {
+      overlayCardElement.style.width = width + 'px';
+      overlayCardElement.style.height = height + 'px';
+    }
+
+    const translateX = targetRect.width - dragRect.width;
+    const translateY = targetRect.height - dragRect.height;
+    resizeHandleElement.style.transform = `translate3d(${translateX}px, ${translateY}px, 0)`;
+  }
+
+  removePositionStyles() {
+    const resizeHandleElement = this.resizeHandleCorner.nativeElement;
+    resizeHandleElement.style.bottom = null;
+    resizeHandleElement.style.right = null;
+    const overlayCardElement = this.overlayCard.nativeElement;
+
+    const dragRect = resizeHandleElement.getBoundingClientRect();
+    const targetRect = overlayCardElement.getBoundingClientRect();
+    const translateX = targetRect.width - dragRect.width;
+    const translateY = targetRect.height - dragRect.height;
+    resizeHandleElement.style.transform = `translate3d(${translateX}px, ${translateY}px, 0)`;
+  }
 
 }
