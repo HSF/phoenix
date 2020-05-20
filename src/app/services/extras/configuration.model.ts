9a2d26e54b7b26449aec8081d5f5a3e53aef7273
ginpachi <m.fawaadali98@gmail.com>
2020-05-20 23:22:17 +0500

    Document attribute pipe, configuration, cut, preset view and lhcb loader

--- src/app/services/extras/configuration.model.ts
+++ src/app/services/extras/configuration.model.ts
@@ -2,23 +2,44 @@ import { PresetView } from './preset-view.model';
 import { EventDataLoader } from '../event-data-loader';
 import { PhoenixLoader } from '../loaders/phoenix-loader';
 
+/**
+ * Configuration for preset views and event data loader.
+ */
 export class Configuration {
+  /** Preset views for switching event display camera. */
   presetViews: PresetView[];
+  /** Event data loader responsible for processing and loading event data. */
   eventDataLoader: EventDataLoader;
 
+  /**
+   * Instantiate the Configuration.
+   */
   constructor() {
     this.presetViews = [];
     this.eventDataLoader = new PhoenixLoader();
   }
 
+  /**
+   * Get if there are any preset views.
+   * @returns If there are any preset views.
+   */
   public anyPresetView(): boolean {
     return this.presetViews.length > 0;
   }
 
+  /**
+   * Set the event data loader to be used.
+   * @param eventDataLoader Event data loader responsible for
+   * processing and loading event data.
+   */
   public setEventDataLoader(eventDataLoader: EventDataLoader) {
     this.eventDataLoader = eventDataLoader;
   }
 
+  /**
+   * Get the event data loader responsible for processing and loading event data.
+   * @returns The event data loader.
+   */
   public getEventDataLoader(): EventDataLoader {
     return this.eventDataLoader;
   }
