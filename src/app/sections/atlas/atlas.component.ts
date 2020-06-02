b0785b446f384d2159d112749b48d2da6bdd8425
ginpachi <m.fawaadali98@gmail.com>
2020-06-02 19:58:29 +0500

    Add icons to preset views and move orthographic view toggle and misc improvements

--- src/app/sections/atlas/atlas.component.ts
+++ src/app/sections/atlas/atlas.component.ts
@@ -20,9 +20,9 @@ export class AtlasComponent implements OnInit {
 
     const configuration = new Configuration();
     configuration.presetViews = [
-      new PresetView('Left View', [0, 0, -12000], 'left'),
-      new PresetView('Center View', [-500, 12000, 0], 'circle'),
-      new PresetView('Right View', [0, 0, 12000], 'right')
+      new PresetView('Left View', [0, 0, -12000], 'left-cube'),
+      new PresetView('Center View', [-500, 12000, 0], 'top-cube'),
+      new PresetView('Right View', [0, 0, 12000], 'right-cube')
     ];
 
     this.eventDisplay.init(configuration);
