648a422ef61ff96b88191399af4598f80563bbac
Emilio <emiliocortina@gmail.com>
2020-06-18 17:18:40 +0200

    Finishing tree UI with toggles

--- src/app/components/ui-menu/tracer-menu/tracer-menu-item/tracer-menu-item.component.ts
+++ src/app/components/ui-menu/tracer-menu/tracer-menu-item/tracer-menu-item.component.ts
@@ -1,19 +1,26 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, AfterViewInit, Input, ViewChildren, QueryList } from '@angular/core';
 
 @Component({
   selector: 'app-tracer-menu-item',
   templateUrl: './tracer-menu-item.component.html',
   styleUrls: ['./tracer-menu-item.component.scss']
 })
-export class TracerMenuItemComponent implements OnInit {
+export class TracerMenuItemComponent implements AfterViewInit {
 
+  @ViewChildren(TracerMenuItemComponent) children!: QueryList<TracerMenuItemComponent>;
   @Input() node: { name: string, geometryId: string, children: [] };
   isExpanded = false;
-  children: TracerMenuItemComponent[];
+  visible = true;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+  }
+
+  public toggleVisibility(visible: boolean) {
+    console.log('hi! it\'s ' + this.node.name);
+    this.visible = visible;
+    this.children.forEach(child => child.toggleVisibility(visible));
   }
 
 }
