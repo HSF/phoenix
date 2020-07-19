50983f092a5fa8e38419b11897b7e095d1ed6b7c
Fawad Ali <m.fawaadali98@gmail.com>
2020-07-19 21:00:53 +0500

    Make a custom function for loading scripts

--- src/app/services/loaders/jsroot-loader.ts
+++ src/app/services/loaders/jsroot-loader.ts
@@ -1,4 +1,4 @@
-import { ScriptService } from "ngx-script-loader";
+import { resolveSrv } from "dns";
 
 /** Global JSROOT variable for accessing functions after loading JSRoot scripts. */
 declare const JSROOT: any;
@@ -9,11 +9,40 @@ declare const JSROOT: any;
  */
 export function loadJSRootScripts(onScriptsLoaded: (JSROOT: any) => void) {
   (async () => {
-    const scriptService = new ScriptService(document);
     const allScripts = ['JSRootCore.js', 'three.min.js', 'ThreeCSG.js', 'd3.min.js', 'JSRootPainter.js', 'JSRootGeoBase.js'];
     for (const script of allScripts) {
-      await scriptService.loadScript('assets/jsroot/' + script).toPromise();
+      await loadScript('assets/jsroot/' + script);
     }
     onScriptsLoaded(JSROOT);
   })();
+}
+
+/**
+ * Load a script dynamically from a URL.
+ * @param scriptURL URL of the script to be loaded.
+ * @param parentElement Parent element to which the script is to be appended.
+ * Defaults to `<head>` tag.
+ */
+function loadScript(scriptURL: string,
+  parentElement: HTMLElement = document.getElementsByTagName('head')[0]) {
+  return new Promise((resolve, reject) => {
+    const scriptExists = document.querySelectorAll('script[src="' + scriptURL + '"]');
+    // If no script exists - add one
+    if (scriptExists.length === 0) {
+      const scriptElement = document.createElement('script');
+      scriptElement.type = 'text/javascript';
+      scriptElement.src = scriptURL;
+      scriptElement.setAttribute('data-scriptFor', 'JSROOT');
+      scriptElement.onload = () => {
+        resolve();
+      }
+      scriptElement.onerror = (event) => {
+        console.error(event);
+        reject();
+      }
+      parentElement.appendChild(scriptElement);
+    } else {
+      resolve();
+    }
+  });
 }
