8b103b1d99852c37cd8c51f6e079eca95d085f4c
Fawad Ali <m.fawaadali98@gmail.com>
2020-08-05 18:30:36 +0500

    Tests for AutoRotate and MainViewToggle components

--- src/app/components/ui-menu/auto-rotate/auto-rotate.component.spec.ts
+++ src/app/components/ui-menu/auto-rotate/auto-rotate.component.spec.ts
@@ -2,17 +2,24 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AutoRotateComponent } from './auto-rotate.component';
 import { AppModule } from '../../../app.module';
+import { UIService } from '../../../services/ui.service';
 
 describe('AutoRotateComponent', () => {
   let component: AutoRotateComponent;
   let fixture: ComponentFixture<AutoRotateComponent>;
 
+  let mockUIService = jasmine.createSpyObj('UIServicie', ['setAutoRotate']);
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [AppModule],
-      declarations: [ AutoRotateComponent ]
+      providers: [{
+        provide: UIService,
+        useValue: mockUIService
+      }],
+      declarations: [AutoRotateComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -23,5 +30,12 @@ describe('AutoRotateComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should toggle auto rotate', () => {
+    expect(component.autoRotate).toBe(false);
+    component.toggleAutoRotate();
+    expect(component.autoRotate).toBe(true);
+    expect(mockUIService.setAutoRotate).toHaveBeenCalled();
   });
 });
--- src/app/components/ui-menu/main-view-toggle/main-view-toggle.component.spec.ts
+++ src/app/components/ui-menu/main-view-toggle/main-view-toggle.component.spec.ts
@@ -2,17 +2,24 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MainViewToggleComponent } from './main-view-toggle.component';
 import { AppModule } from '../../../app.module';
+import { UIService } from '../../../services/ui.service';
 
 describe('MainViewToggleComponent', () => {
   let component: MainViewToggleComponent;
   let fixture: ComponentFixture<MainViewToggleComponent>;
 
+  let mockUIService = jasmine.createSpyObj('UIServicie', ['toggleOrthographicView']);
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [AppModule],
-      declarations: [ MainViewToggleComponent ]
+      providers: [{
+        provide: UIService,
+        useValue: mockUIService
+      }],
+      declarations: [MainViewToggleComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -23,5 +30,12 @@ describe('MainViewToggleComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should switch main view', () => {
+    expect(component.orthographicView).toBe(false);
+    component.switchMainView();
+    expect(component.orthographicView).toBe(true);
+    expect(mockUIService.toggleOrthographicView).toHaveBeenCalled();
   });
 });
