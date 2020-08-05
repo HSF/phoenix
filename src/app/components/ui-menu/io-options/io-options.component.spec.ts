a4e35b7a501bc999e3e58023331be921eafc4114
Fawad Ali <m.fawaadali98@gmail.com>
2020-08-05 20:03:58 +0500

    Tests for ObjectSelection, IoOptions, InfoPanel, EventSelector, DarkTheme and CollectionsInfo components

--- src/app/components/ui-menu/io-options/io-options.component.spec.ts
+++ src/app/components/ui-menu/io-options/io-options.component.spec.ts
@@ -2,16 +2,22 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { IoOptionsComponent } from './io-options.component';
 import { AppModule } from 'src/app/app.module';
+import { MatDialog } from '@angular/material/dialog';
 
 describe('IoOptionsComponent', () => {
   let component: IoOptionsComponent;
   let fixture: ComponentFixture<IoOptionsComponent>;
 
+  let dialog: MatDialog;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [AppModule]
+      imports: [AppModule],
+      providers: [MatDialog],
+      declarations: [IoOptionsComponent]
     })
       .compileComponents();
+    dialog = TestBed.get(MatDialog);
   }));
 
   beforeEach(() => {
@@ -22,5 +28,12 @@ describe('IoOptionsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should open IO dialog', () => {
+    spyOn(dialog, 'open');
+
+    component.openIODialog();
+    expect(dialog.open).toHaveBeenCalled();
   });
 });
--- src/app/components/ui-menu/io-options/io-options.component.ts
+++ src/app/components/ui-menu/io-options/io-options.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { IOOptionsDialogComponent } from './io-options-dialog/io-options-dialog.component';
 
@@ -7,12 +7,9 @@ import { IOOptionsDialogComponent } from './io-options-dialog/io-options-dialog.
   templateUrl: './io-options.component.html',
   styleUrls: ['./io-options.component.scss']
 })
-export class IoOptionsComponent implements OnInit {
+export class IoOptionsComponent {
 
   constructor(private dialog: MatDialog) { }
-
-  ngOnInit() {
-  }
 
   openIODialog() {
     this.dialog.open(IOOptionsDialogComponent, { panelClass: 'dialog' });
