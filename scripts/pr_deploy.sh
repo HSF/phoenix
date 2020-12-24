64acd748759024f8f908b6d9c4053119ad98a510
Fawad Ali <m.fawaadali98@gmail.com>
2020-12-24 14:05:38 +0500

    fix(app): fix PR status update

--- scripts/pr_deploy.sh
+++ scripts/pr_deploy.sh
@@ -4,9 +4,13 @@ if [ "$TRAVIS_PULL_REQUEST" != false ]
 then
 
 PR_DOMAIN=http://phoenix-pr-${TRAVIS_PULL_REQUEST}.surge.sh
-GH_API_PR=https://api.github.com/repos/HSF/phoenix/pulls/${TRAVIS_PULL_REQUEST}
+GH_API_PR=https://api.github.com/repos/${TRAVIS_REPO_SLUG}/statuses/${TRAVIS_PULL_REQUEST_SHA}
+
+echo "Deploying PR to surge"
 
 surge --project ./packages/phoenix-ng/docs/ --domain ${PR_DOMAIN};
+
+echo "Updating PR status"
 
 curl -H "Authorization: token ${GH_TOKEN}" --request POST ${GH_API_PR} --data '{"state": "success", "target_url": "'${PR_DOMAIN}'", "description": "Pull request deployed", "context": "CollectiveDynamicDeploy"}'
 
