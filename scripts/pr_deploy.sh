613bfce2734510504408f03b841ad4bbefff04c8
Fawad Ali <m.fawaadali98@gmail.com>
2020-12-24 06:25:52 +0500

    fix(app): surge fixes

--- scripts/pr_deploy.sh
+++ scripts/pr_deploy.sh
@@ -6,7 +6,7 @@ then
 PR_DOMAIN=http://phoenix-pr-${TRAVIS_PULL_REQUEST}.surge.sh
 GH_API_PR=https://api.github.com/repos/HSF/phoenix/pulls/${TRAVIS_PULL_REQUEST}
 
-surge --project ../packages/phoenix-ng/docs/ --domain ${PR_DOMAIN};
+surge --project ./packages/phoenix-ng/docs/ --domain ${PR_DOMAIN};
 
 curl -H "Authorization: token ${GH_TOKEN}" --request POST ${GH_API_PR} --data '{"state": "success", "target_url": "'${PR_DOMAIN}'", "description": "Pull request deployed", "context": "CollectiveDynamicDeploy"}'
 
