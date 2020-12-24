3cb36a280ccdbe3f2b1ff0e116fa50ecaadc9649
Fawad Ali <m.fawaadali98@gmail.com>
2020-12-24 06:04:44 +0500

    feat(app): complete initial surge deployment set up

--- scripts/pr_deploy.sh
+++ scripts/pr_deploy.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-PR_DOMAIN=phoenix-pr-${TRAVIS_PULL_REQUEST}.surge.sh
+if [ "$TRAVIS_PULL_REQUEST" != false ]
+then
+
+PR_DOMAIN=http://phoenix-pr-${TRAVIS_PULL_REQUEST}.surge.sh
+GH_API_PR=https://api.github.com/repos/HSF/phoenix/pulls/${TRAVIS_PULL_REQUEST}
+
+surge --project ../packages/phoenix-ng/docs/ --domain ${PR_DOMAIN};
+
+curl -H "Authorization: token ${GH_TOKEN}" --request POST ${GH_API_PR} --data '{"state": "success", "target_url": "'${PR_DOMAIN}'", "description": "Pull request deployed", "context": "CollectiveDynamicDeploy"}'
+
+fi
