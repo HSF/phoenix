1a3b1e4767eed1430b0a90e73f3f1058ae007196
Fawad Ali <m.fawaadali98@gmail.com>
2020-12-31 22:09:09 +0500

    build(): setting up karma for testing

---
+++ packages/phoenix-event-display/karma.conf.js
@@ -0,0 +1,14 @@
+module.exports = function (config) {
+  config.set({
+    frameworks: ["jasmine", "karma-typescript"],
+    files: [
+      { pattern: "src/tests/**/*.ts" }
+    ],
+    preprocessors: {
+      "**/*.ts": ["karma-typescript"]
+    },
+    reporters: ["dots", "karma-typescript"],
+    browsers: ["ChromeHeadless"],
+    singleRun: true
+  });
+};
