c3a97b81b9d4f25874392638979dd5ad981a3a90
Fawad Ali <m.fawaadali98@gmail.com>
2021-03-09 00:11:57 +0500

    build(): use action for coveralls

--- packages/phoenix-event-display/configs/karma.conf.js
+++ packages/phoenix-event-display/configs/karma.conf.js
@@ -21,11 +21,11 @@ module.exports = function (config) {
       reports: {
         html: {
           directory: 'coverage',
-          subdirectory: 'phoenix-event-display'
+          subdirectory: '.'
         },
         lcovonly: {
           directory: 'coverage',
-          subdirectory: 'phoenix-event-display'
+          subdirectory: '.'
         }
       },
       compilerOptions: {
