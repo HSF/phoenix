45a0123892092c9397a80b5c99bb9d5c156aa0b1
Fawad Ali <m.fawaadali98@gmail.com>
2021-04-16 23:15:06 +0500

    feat(event-display): split dat.GUI menu options

--- packages/phoenix-event-display/src/ui/dat-gui-ui.ts
+++ packages/phoenix-event-display/src/ui/dat-gui-ui.ts
@@ -20,6 +20,15 @@ export class DatGUIMenuUI {
   private eventFolder: any;
   /** dat.GUI menu folder containing labels. */
   private labelsFolder: any;
+  /** If the geometry folder is added or not */
+  private geomFolderAdded: boolean = false;
+
+  /** Max changeable position of an object along the x-axis. */
+  private maxPositionX = 4000;
+  /** Max changeable position of an object along the y-axis. */
+  private maxPositionY = 4000;
+  /** Max changeable position of an object along the z-axis. */
+  private maxPositionZ = 4000;
 
   /**
    * Create dat.GUI menu with different controls related to detector geometry and event data.
@@ -64,5 +73,103 @@ export class DatGUIMenuUI {
     wireframeGeometriesMenu.onChange((value) => {
       this.three.getSceneManager().wireframeGeometries(value);
     });
+  }
+
+  /**
+   * Adds geometry to the dat.GUI menu's geometry folder and sets up its configurable options.
+   * @param name Name of the geometry.
+   * @param color Color of the geometry.
+   * @param menuNodeName Name of the node in Phoenix menu to add the geometry to.
+   * @param initiallyVisible Whether the geometry is initially visible or not.
+   */
+  public addGeometry(
+    name: string,
+    color: any,
+    menuNodeName?: string,
+    initiallyVisible: boolean = true
+  ) {
+    if (!this.geomFolderAdded) {
+      this.addGeomFolder();
+    }
+
+    // A new folder for the object is added to the 'Geometry' folder
+    this.guiParameters[name] = {
+      show: initiallyVisible,
+      color: color ?? '#000000',
+      x: 0,
+      y: 0,
+      z: 0,
+      detectorOpacity: 1.0,
+      remove: this.removeOBJ(name),
+      scale: 1,
+    };
+
+    const objFolder = this.geomFolder.addFolder(name);
+    // A color picker is added to the object's folder
+    const colorMenu = objFolder
+      .addColor(this.guiParameters[name], 'color')
+      .name('Color');
+    colorMenu.onChange((value) =>
+      this.three.getSceneManager().changeObjectColor(name, value)
+    );
+
+    const opacity = objFolder
+      .add(this.guiParameters[name], 'detectorOpacity', 0.0, 1.0)
+      .name('Opacity');
+    opacity.onFinishChange((newValue) =>
+      this.three.getSceneManager().setGeometryOpacity(name, newValue)
+    );
+
+    // A boolean toggle for showing/hiding the object is added to its folder
+    const showMenu = objFolder
+      .add(this.guiParameters[name], 'show')
+      .name('Show')
+      .listen();
+    showMenu.onChange((value) =>
+      this.three.getSceneManager().objectVisibility(name, value)
+    );
+    // Scale slider
+    const scaleMenu = objFolder
+      .add(this.guiParameters[name], 'scale', 0, 1000)
+      .name('Scale');
+    scaleMenu.onChange((value) => {
+      this.three.getSceneManager().scaleObject(name, value);
+    });
+    // Controls for positioning.
+    // const position = this.three.getObjectPosition(name);
+    objFolder
+      .add(this.guiParameters[name], 'x', -this.maxPositionX, this.maxPositionX)
+      .name('X')
+      .onChange((value) =>
+        this.three.getSceneManager().getObjectPosition(name).setX(value)
+      );
+    objFolder
+      .add(this.guiParameters[name], 'y', -this.maxPositionY, this.maxPositionY)
+      .name('Y')
+      .onChange((value) =>
+        this.three.getSceneManager().getObjectPosition(name).setY(value)
+      );
+    objFolder
+      .add(this.guiParameters[name], 'z', -this.maxPositionZ, this.maxPositionZ)
+      .name('Z')
+      .onChange((value) =>
+        this.three.getSceneManager().getObjectPosition(name).setZ(value)
+      );
+    // Controls for deleting the obj
+    objFolder.add(this.guiParameters[name], 'remove').name('Remove');
+  }
+
+  /**
+   * Remove object from the dat.GUI menu.
+   * @param name Name of the object to be removed.
+   */
+  private removeOBJ(name: string) {
+    return () => {
+      const folder = this.geomFolder.__folders[name];
+      if (folder) {
+        this.geomFolder.removeFolder(folder);
+      }
+      this.three.getSceneManager().removeGeometry(name);
+    };
   }
 }
--- packages/phoenix-event-display/src/ui/index.ts
+++ packages/phoenix-event-display/src/ui/index.ts
@@ -36,13 +36,6 @@ export class UIManager {
   private canvas: HTMLElement;
   /** If dark theme is enabled or disabled. */
   private darkTheme: boolean;
-
-  /** Max changeable position of an object along the x-axis. */
-  private maxPositionX = 4000;
-  /** Max changeable position of an object along the y-axis. */
-  private maxPositionY = 4000;
-  /** Max changeable position of an object along the z-axis. */
-  private maxPositionZ = 4000;
   /** Root node of the phoenix menu. */
   private phoenixMenu: PhoenixMenuNode;
 
@@ -174,28 +167,6 @@ export class UIManager {
     this.geomFolderAdded = true;
 
     if (this.hasDatGUIMenu) {
-      if (this.geomFolder === null) {
-        this.geomFolder = this.gui.addFolder(SceneManager.GEOMETRIES_ID);
-      }
-      this.guiParameters.geometries = { show: true, wireframe: false };
-      // A boolean toggle for showing/hiding the geometries is added to the 'Geometry' folder.
-      const showGeometriesMenu = this.geomFolder
-        .add(this.guiParameters.geometries, 'show')
-        .name('Show')
-        .listen();
-      showGeometriesMenu.onChange((value) => {
-        this.three
-          .getSceneManager()
-          .objectVisibility(SceneManager.GEOMETRIES_ID, value);
-      });
-      // A boolean toggle for enabling/disabling the geometries' wireframing.
-      const wireframeGeometriesMenu = this.geomFolder
-        .add(this.guiParameters.geometries, 'wireframe')
-        .name('Wireframe')
-        .listen();
-      wireframeGeometriesMenu.onChange((value) => {
-        this.three.getSceneManager().wireframeGeometries(value);
-      });
     }
 
     if (this.hasPhoenixMenu) {
@@ -273,86 +244,6 @@ export class UIManager {
     }
 
     if (this.hasDatGUIMenu) {
-      // A new folder for the object is added to the 'Geometry' folder
-      this.guiParameters[name] = {
-        show: initiallyVisible,
-        color: color ?? '#000000',
-        x: 0,
-        y: 0,
-        z: 0,
-        detectorOpacity: 1.0,
-        remove: this.removeOBJ(name),
-        scale: 1,
-      };
-
-      const objFolder = this.geomFolder.addFolder(name);
-      // A color picker is added to the object's folder
-      const colorMenu = objFolder
-        .addColor(this.guiParameters[name], 'color')
-        .name('Color');
-      colorMenu.onChange((value) =>
-        this.three.getSceneManager().changeObjectColor(name, value)
-      );
-
-      const opacity = objFolder
-        .add(this.guiParameters[name], 'detectorOpacity', 0.0, 1.0)
-        .name('Opacity');
-      opacity.onFinishChange((newValue) =>
-        this.three.getSceneManager().setGeometryOpacity(name, newValue)
-      );
-
-      // A boolean toggle for showing/hiding the object is added to its folder
-      const showMenu = objFolder
-        .add(this.guiParameters[name], 'show')
-        .name('Show')
-        .listen();
-      showMenu.onChange((value) =>
-        this.three.getSceneManager().objectVisibility(name, value)
-      );
-      // Scale slider
-      const scaleMenu = objFolder
-        .add(this.guiParameters[name], 'scale', 0, 1000)
-        .name('Scale');
-      scaleMenu.onChange((value) => {
-        this.three.getSceneManager().scaleObject(name, value);
-      });
-      // Controls for positioning.
-      // const position = this.three.getObjectPosition(name);
-      objFolder
-        .add(
-          this.guiParameters[name],
-          'x',
-          -this.maxPositionX,
-          this.maxPositionX
-        )
-        .name('X')
-        .onChange((value) =>
-          this.three.getSceneManager().getObjectPosition(name).setX(value)
-        );
-      objFolder
-        .add(
-          this.guiParameters[name],
-          'y',
-          -this.maxPositionY,
-          this.maxPositionY
-        )
-        .name('Y')
-        .onChange((value) =>
-          this.three.getSceneManager().getObjectPosition(name).setY(value)
-        );
-      objFolder
-        .add(
-          this.guiParameters[name],
-          'z',
-          -this.maxPositionZ,
-          this.maxPositionZ
-        )
-        .name('Z')
-        .onChange((value) =>
-          this.three.getSceneManager().getObjectPosition(name).setZ(value)
-        );
-      // Controls for deleting the obj
-      objFolder.add(this.guiParameters[name], 'remove').name('Remove');
     }
 
     if (this.hasPhoenixMenu) {
@@ -384,20 +275,6 @@ export class UIManager {
           },
         });
     }
-  }
-
-  /**
-   * Remove object from the dat.GUI menu.
-   * @param name Name of the object to be removed.
-   */
-  private removeOBJ(name: string) {
-    return () => {
-      const folder = this.geomFolder.__folders[name];
-      if (folder) {
-        this.geomFolder.removeFolder(folder);
-      }
-      this.three.getSceneManager().removeGeometry(name);
-    };
   }
 
   /**
