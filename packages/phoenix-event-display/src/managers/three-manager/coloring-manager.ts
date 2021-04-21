9222e04c295ac674f015d43bde2ae2a90ccbf61d
Fawad Ali <m.fawaadali98@gmail.com>
2021-04-21 22:18:48 +0200

    feat(event-display): implement color by momentum

--- packages/phoenix-event-display/src/managers/three-manager/coloring-manager.ts
+++ packages/phoenix-event-display/src/managers/three-manager/coloring-manager.ts
@@ -14,34 +14,17 @@ export class ColoringManager {
    * Color objects by a property's value or range.
    * @param color Color to set for the object.
    * @param objectsGroup Name of the object(s) group to color.
-   * @param propertyName Name of the property by which to color the object.
-   * @param value Value to check in the property for coloring.
    * @param customCheck Function to custom check values against object params.
    */
   colorObjectsByProperty(
     color: any,
     objectsGroup: string,
-    propertyName?: string,
-    value?: [number, number] | number | string,
-    customCheck?: (objectUserData: any) => boolean
+    customCheck: (objectUserData: any) => boolean
   ) {
     const objects = this.sceneManager.getScene().getObjectByName(objectsGroup);
     objects.traverse((object: any) => {
-      if (object.material?.color) {
-        if (value !== undefined) {
-          if (Array.isArray(value)) {
-            if (
-              object.userData?.[propertyName] >= value[0] &&
-              object.userData?.[propertyName] <= value[1]
-            ) {
-              object.material.color.set(color);
-            }
-          } else if (object.userData?.[propertyName] === value) {
-            object.material.color.set(color);
-          }
-        } else if (customCheck?.(object.userData)) {
-          object.material.color.set(color);
-        }
+      if (object.material?.color && customCheck?.(object.userData)) {
+        object.material.color.set(color);
       }
     });
   }
--- packages/phoenix-event-display/src/managers/ui-manager/coloring-options.ts
+++ packages/phoenix-event-display/src/managers/ui-manager/coloring-options.ts
@@ -2,6 +2,28 @@ import { PrettySymbols } from '../../helpers/pretty-symbols';
 import { ThreeManager } from '../three-manager';
 import { PhoenixMenuNode } from './phoenix-menu/phoenix-menu-node';
 
+// Helper functions
+
+/**
+ * Color charge based on charge value.
+ * @param objectParams Object parameters associated to the 3D object.
+ * @param chargeValue Value of charge (-1, 0, 1).
+ * @returns Whether the charge is equal to the value.
+ */
+const colorCharge = (objectParams: any, chargeValue: number) => {
+  // For ATLAS data, the charge is calculated from dparams[4] otherwise it exists as an object's userData
+  if (Math.sign(1 / parseInt(objectParams?.dparams?.[4])) === chargeValue) {
+    return true;
+  } else if (objectParams?.charge === chargeValue) {
+    return true;
+  }
+};
+
+const getMomentum = (objectParams: any) =>
+  objectParams?.dparams?.[4]
+    ? Math.abs(1 / parseFloat(objectParams?.dparams?.[4]))
+    : objectParams?.mom;
+
 /**
  * Function to add coloring options to tracks.
  * @param threeManager Three manager for three.js operations.
@@ -13,36 +35,33 @@ export const tracksColoringOptions = (
 ) => {
   // Variables
   const collectionName = collectionFolder.name;
+
   const colorByOptions = {
     charge: 'Charge ' + PrettySymbols.getPrettySymbol('charge'),
     mom: 'Momentum ' + PrettySymbols.getPrettySymbol('charge'),
   };
+
   const chargeColors = {
     '-1': '#ff0000',
     '0': '#ff0000',
     '1': '#ff0000',
   };
+
   const momColors = {
     min: {
       value: 0,
       color: '#ff0000',
     },
     max: {
-      value: 15 * Math.pow(10, 9),
+      value: 50000,
       color: '#ff0000',
     },
   };
-  let selectedColorByOption = colorByOptions.charge;
 
-  // Helper functions
-  const colorCharge = (objectParams: any, chargeValue: number) => {
-    // For ATLAS data, the charge is calculated from dparams[4] otherwise it exists as an object's userData
-    if (Math.sign(1 / parseInt(objectParams?.dparams?.[4])) === chargeValue) {
-      return true;
-    } else if (objectParams?.charge === chargeValue) {
-      return true;
-    }
-  };
+  const momMidValue = (min: number, max: number) =>
+    (momColors.min.value + momColors.max.value) / 2;
+
+  let selectedColorByOption = colorByOptions.charge;
 
   // Configurations
   collectionFolder.addConfig('label', {
@@ -63,13 +82,13 @@ export const tracksColoringOptions = (
               .colorObjectsByProperty(
                 chargeColors[chargeValue],
                 collectionName,
-                undefined,
-                undefined,
                 (objectUserData) => colorCharge(objectUserData, chargeValue)
               );
           });
           break;
         case colorByOptions.mom:
+          colorByMomentum('min', momColors.min.color);
+          colorByMomentum('max', momColors.max.color);
           break;
       }
     },
@@ -86,38 +105,76 @@ export const tracksColoringOptions = (
         if (selectedColorByOption === colorByOptions.charge) {
           threeManager
             .getColoringManager()
-            .colorObjectsByProperty(
-              color,
-              collectionName,
-              undefined,
-              undefined,
-              (objectUserData) => colorCharge(objectUserData, chargeValue)
+            .colorObjectsByProperty(color, collectionName, (objectUserData) =>
+              colorCharge(objectUserData, chargeValue)
             );
         }
       },
     });
   });
 
+  // Momentum helper functions
+  const colorByMomentum = (minOrMax: string, color: any) => {
+    threeManager
+      .getColoringManager()
+      .colorObjectsByProperty(color, 'Tracks', (objectParams) => {
+        const mom = getMomentum(objectParams);
+        const mid = momMidValue(momColors.min.value, momColors.max.value);
+
+        if (minOrMax === 'max' && mom > mid && mom < momColors.max.value) {
+          return true;
+        } else if (
+          minOrMax === 'min' &&
+          mom < mid &&
+          mom > momColors.min.value
+        ) {
+          return true;
+        }
+      });
+  };
+
   // Momentum configurations
-  collectionFolder.addConfig('rangeSlider', {
-    label: PrettySymbols.getPrettySymbol('mom') + ' color range',
+  collectionFolder.addConfig('slider', {
+    label: PrettySymbols.getPrettySymbol('mom') + ' min',
     min: momColors.min.value,
     max: momColors.max.value,
-    step: 10,
     value: momColors.min.value,
-    highValue: momColors.max.value,
-    onChange: ({ value, highValue }) => {
+    step: 10,
+    allowCustomValue: true,
+    onChange: (value: number) => {
       momColors.min.value = value;
-      momColors.max.value = highValue;
+      if (selectedColorByOption === colorByOptions.mom) {
+        colorByMomentum('min', momColors.min.color);
+        colorByMomentum('max', momColors.max.color);
+      }
+    },
+  });
+  collectionFolder.addConfig('slider', {
+    label: PrettySymbols.getPrettySymbol('mom') + ' max',
+    min: momColors.min.value,
+    max: momColors.max.value,
+    value: momColors.max.value,
+    step: 10,
+    allowCustomValue: true,
+    onChange: (value: number) => {
+      momColors.max.value = value;
+      if (selectedColorByOption === colorByOptions.mom) {
+        colorByMomentum('min', momColors.min.color);
+        colorByMomentum('max', momColors.max.color);
+      }
     },
   });
 
   Object.entries(momColors).forEach(([key, value]) => {
     collectionFolder.addConfig('color', {
-      label: PrettySymbols.getPrettySymbol('mom') + ' ' + key,
+      label: PrettySymbols.getPrettySymbol('mom') + ' ' + key + ' color',
       color: value.color,
       onChange: (color: any) => {
         value.color = color;
+
+        if (selectedColorByOption === colorByOptions.mom) {
+          colorByMomentum(key, color);
+        }
       },
     });
   });
