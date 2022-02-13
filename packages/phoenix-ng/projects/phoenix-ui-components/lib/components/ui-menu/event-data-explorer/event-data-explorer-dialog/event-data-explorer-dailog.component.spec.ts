d16d939dcdc7fe2e59e1d9144a9b9930f2803080
Fawad Ali <m.fawaadali98@gmail.com>
2022-02-13 18:37:21 +0500

    build(app): write tests for event data explorer

--- packages/phoenix-ng/projects/phoenix-ui-components/lib/components/ui-menu/event-data-explorer/event-data-explorer-dialog/event-data-explorer-dailog.component.spec.ts
+++ packages/phoenix-ng/projects/phoenix-ui-components/lib/components/ui-menu/event-data-explorer/event-data-explorer-dialog/event-data-explorer-dailog.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { EventDisplayService } from '../../../../services/event-display.service';
+import { FileNode } from '../../../file-explorer/file-explorer.component';
 import { PhoenixUIModule } from '../../../phoenix-ui.module';
 import { EventDataExplorerDialogData } from '../event-data-explorer.component';
 import {
@@ -13,35 +14,44 @@ fdescribe('EventDataExplorerDialogComponent', () => {
   let component: EventDataExplorerDialogComponent;
   let fixture: ComponentFixture<EventDataExplorerDialogComponent>;
 
-  const mockEventDisplay = jasmine.createSpyObj('EventDisplayService', [
-    'parsePhoenixEvents',
-    'buildEventDataFromJSON',
-    'getStateManager',
+  const mockStateManager = jasmine.createSpyObj('StateManager', [
+    'loadStateFromJSON',
   ]);
+  const mockEventDisplay = jasmine.createSpyObj(
+    'EventDisplayService',
+    ['parsePhoenixEvents', 'buildEventDataFromJSON'],
+    {
+      getStateManager: () => mockStateManager,
+    }
+  );
   const mockDialogRef = jasmine.createSpyObj('MatDialogRef', ['close']);
   const mockDialogData: EventDataExplorerDialogData = {
     apiURL: 'http://localhost',
   };
 
+  const mockFileResponse: FileResponse[] = [
+    {
+      name: 'event_data/test.json',
+      url: 'https://example.com/event_data/test.json',
+    },
+    {
+      name: 'event_data/test.xml',
+      url: 'https://example.com/event_data/test.xml',
+    },
+    {
+      name: 'config_data/test.json',
+      url: 'https://example.com/config_data/test.json',
+    },
+  ];
+  const mockResponse = new Response(JSON.stringify(mockFileResponse), {
+    status: 200,
+  });
+
   beforeAll(() => {
-    const responseInit: ResponseInit = {
-      status: 200,
-    };
-    const mockData: FileResponse[] = [
-      {
-        name: 'event_data/test.json',
-        url: 'https://example.com/event_data/test.json',
-      },
-      {
-        name: 'config_data/test.json',
-        url: 'https://example.com/config_data/test.json',
-      },
-    ];
-
-    const response = new Response(JSON.stringify(mockData), responseInit);
-
-    spyOn(response, 'json').and.returnValue(Promise.resolve(mockData));
-    spyOn(window, 'fetch').and.returnValue(Promise.resolve(response));
+    spyOn(mockResponse, 'json').and.returnValue(
+      Promise.resolve(mockFileResponse)
+    );
+    spyOn(window, 'fetch').and.returnValue(Promise.resolve(mockResponse));
   });
 
   beforeEach(() => {
@@ -73,5 +83,50 @@ fdescribe('EventDataExplorerDialogComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should build file node from response', () => {
+    const fileNode: FileNode = (component as any).buildFileNode(
+      mockFileResponse
+    );
+
+    expect(fileNode.children['event_data'].children['test.json'].name).toBe(
+      'test.json'
+    );
+    expect(fileNode.children['config_data'].children['test.json'].name).toBe(
+      'test.json'
+    );
+  });
+
+  it('should load event based on file type', () => {
+    spyOn(
+      EventDataExplorerDialogComponent.prototype,
+      'makeRequest'
+    ).and.callFake(
+      (_arg1: string, _arg2: string, onData: (data: string) => void) =>
+        onData('test')
+    );
+
+    spyOn(component as any, 'loadJSONEvent').and.stub();
+    component.loadEvent('https://example.com/event_data/test.json');
+    expect((component as any).loadJSONEvent).toHaveBeenCalled();
+
+    spyOn(component as any, 'loadJiveXMLEvent').and.stub();
+    component.loadEvent('https://example.com/event_data/test.xml');
+    expect((component as any).loadJiveXMLEvent).toHaveBeenCalled();
+  });
+
+  it('should load config', () => {
+    spyOn(
+      EventDataExplorerDialogComponent.prototype,
+      'makeRequest'
+    ).and.callFake(
+      (_arg1: string, _arg2: string, onData: (data: string) => void) =>
+        onData('{}')
+    );
+
+    component.loadConfig('https://example.com/config_data/test.json');
+
+    expect(mockStateManager.loadStateFromJSON).toHaveBeenCalledWith({});
   });
 });
