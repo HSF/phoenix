78a7e7d360348a30838661a0a1319befe8f10a16
Fawad Ali <m.fawaadali98@gmail.com>
2022-05-27 01:01:56 +0500

    Add tests for cycle events component

--- packages/phoenix-ng/projects/phoenix-ui-components/lib/components/ui-menu/cycle-events/cycle-events.component.spec.ts
+++ packages/phoenix-ng/projects/phoenix-ui-components/lib/components/ui-menu/cycle-events/cycle-events.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { EventDisplayService, PhoenixUIModule } from 'phoenix-ui-components';
 
 import { CycleEventsComponent } from './cycle-events.component';
 
@@ -6,11 +7,24 @@ describe('CycleEventsComponent', () => {
   let component: CycleEventsComponent;
   let fixture: ComponentFixture<CycleEventsComponent>;
 
+  const mockEventDisplay = jasmine.createSpyObj('EventDisplayService', {
+    listenToLoadedEventsChange: jasmine.createSpy().and.callFake((callback) => {
+      callback(['eventKey1', 'eventKey2']);
+    }),
+    loadEvent: jasmine.createSpy().and.stub(),
+  });
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CycleEventsComponent ]
-    })
-    .compileComponents();
+      imports: [PhoenixUIModule],
+      providers: [
+        {
+          provide: EventDisplayService,
+          useValue: mockEventDisplay,
+        },
+      ],
+      declarations: [CycleEventsComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +35,22 @@ describe('CycleEventsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should listen to change in events', () => {
+    component.ngOnInit();
+    expect(mockEventDisplay.listenToLoadedEventsChange).toHaveBeenCalled();
+  });
+
+  it('should start rotating through events on toggle', () => {
+    jasmine.clock().install();
+    component.interval = 1000;
+
+    component.toggleCycle();
+    expect(component.active).toBeTrue();
+    jasmine.clock().tick(1200);
+    expect(mockEventDisplay.loadEvent).toHaveBeenCalledWith('eventKey2');
+
+    jasmine.clock().uninstall();
   });
 });
