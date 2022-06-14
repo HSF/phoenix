ea3d1f4392cd5f01cf7517b52fd2f032599f1909
DamianArado <khanhumayun95@gmail.com>
2022-06-14 20:27:31 +0530

    test(event-display): remove jasmine and karma tests setup

--- packages/phoenix-event-display/src/tests/loaders/trackml-loader.spec.ts
+++
@@ -1,7 +0,0 @@
-import { TrackmlLoader } from '../../loaders/trackml-loader';
-
-describe('TrackmlLoader', () => {
-  it('should create an instance', () => {
-    expect(new TrackmlLoader()).toBeTruthy();
-  });
-});
--- packages/phoenix-event-display/src/tests/managers/three-manager/renderer-manager.spec.ts
+++
@@ -1,7 +0,0 @@
-import { RendererManager } from '../../../managers/three-manager/renderer-manager';
-
-describe('RendererManager', () => {
-  it('should create an instance', () => {
-    expect(new RendererManager()).toBeTruthy();
-  });
-});
