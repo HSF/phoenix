66b712b03b79c53c9e41a88a6db66c960e30fcec
Fawad Ali <m.fawaadali98@gmail.com>
2022-06-17 21:47:39 +0500

    Export cycle-events component as public api

--- packages/phoenix-ng/projects/phoenix-ui-components/lib/components/ui-menu/index.ts
+++ packages/phoenix-ng/projects/phoenix-ui-components/lib/components/ui-menu/index.ts
@@ -31,3 +31,4 @@ export * from './share-link/share-link.component';
 export * from './share-link/share-link-dialog/share-link-dialog.component';
 export * from './event-data-explorer/event-data-explorer.component';
 export * from './event-data-explorer/event-data-explorer-dialog/event-data-explorer-dialog.component';
+export * from './cycle-events/cycle-events.component';
