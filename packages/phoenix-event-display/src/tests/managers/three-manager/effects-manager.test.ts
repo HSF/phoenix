0f546e420ecde38a6d26b11743ac11930409d40c
DamianArado <khanhumayun95@gmail.com>
2022-07-06 02:43:06 +0530

    test(event-display): update unit test for EffectsManager

--- packages/phoenix-event-display/src/tests/managers/three-manager/effects-manager.test.ts
+++ packages/phoenix-event-display/src/tests/managers/three-manager/effects-manager.test.ts
@@ -1,7 +1,18 @@
 /**
  * @jest-environment jsdom
  */
-import { Camera, Scene, WebGLRenderer } from 'three';
+import {
+  Camera,
+  Scene,
+  WebGLRenderer,
+  Color,
+  WebGLRenderTarget,
+  Texture,
+  MeshDepthMaterial,
+  ShaderMaterial,
+} from 'three';
+import { OutlinePass } from 'three/examples/jsm/postprocessing/OutlinePass';
+import { FullScreenQuad } from 'three/examples/jsm/postprocessing/Pass';
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
 import { EffectsManager } from '../../../managers/three-manager/effects-manager';
 import createRenderer from '../../helpers/create-renderer';
@@ -23,24 +34,50 @@ describe('EffectsManager', () => {
     expect(effectsManager).toBeTruthy();
   });
 
-  it('should render the effects composer', () => {
-    jest.spyOn(effectsManager, 'addOutlinePassForSelection');
-    effectsManager.addOutlinePassForSelection();
-    expect(effectsManager.addOutlinePassForSelection).toHaveBeenCalled();
+  it('should initialize the outline pass for highlighting hovered over event display elements', () => {
+    const outlinePass = effectsManager.addOutlinePassForSelection();
+
+    expect(outlinePass).toBeInstanceOf(OutlinePass);
+    expect(outlinePass.selectedObjects.length).toBe(0);
+    expect(outlinePass.visibleEdgeColor).toBeInstanceOf(Color);
+    expect(outlinePass.hiddenEdgeColor).toBeInstanceOf(Color);
+
+    expect(outlinePass.edgeGlow).toBe(0);
+    expect(outlinePass.edgeThickness).toBe(1);
+    expect(outlinePass.edgeStrength).toBe(3);
+
+    expect(outlinePass.resolution.x).toBe(window.innerWidth);
+    expect(outlinePass.resolution.y).toBe(window.innerHeight);
+
+    expect(outlinePass.renderTargetMaskBuffer).toBeInstanceOf(
+      WebGLRenderTarget
+    );
+    expect(outlinePass.renderTargetMaskBuffer.texture).toBeInstanceOf(Texture);
+
+    expect(outlinePass.depthMaterial).toBeInstanceOf(MeshDepthMaterial);
+    expect(outlinePass.prepareMaskMaterial).toBeInstanceOf(ShaderMaterial);
+
+    expect(outlinePass.renderTargetDepthBuffer).toBeInstanceOf(
+      WebGLRenderTarget
+    );
+    expect(outlinePass.edgeDetectionMaterial).toBeInstanceOf(ShaderMaterial);
+    expect(outlinePass.overlayMaterial).toBeInstanceOf(ShaderMaterial);
+    expect(outlinePass.materialCopy).toBeInstanceOf(ShaderMaterial);
+    expect(outlinePass.fsQuad).toBeInstanceOf(FullScreenQuad);
   });
 
   it('should remove a pass from the effect composer', () => {
-    jest.spyOn(effectsManager, 'removePass');
-    effectsManager.removePass(new RenderPass(scene, camera));
-    expect(effectsManager.removePass).toHaveBeenCalled();
+    const pass = new RenderPass(scene, camera);
+    effectsManager.removePass(pass);
+
+    expect(effectsManager.composer.passes.length).toBe(0);
   });
 
   it('should set the antialiasing of renderer', () => {
-    jest.spyOn(effectsManager, 'setAntialiasing');
     effectsManager.setAntialiasing(true);
-    expect(effectsManager.setAntialiasing).toHaveBeenCalledWith(true);
+    expect(effectsManager.antialiasing).toBe(true);
 
     effectsManager.setAntialiasing(false);
-    expect(effectsManager.setAntialiasing).toHaveBeenCalledWith(false);
+    expect(effectsManager.antialiasing).toBe(false);
   });
 });
