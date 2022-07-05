e96165caab08ad683b7e101697757143ff4c718e
DamianArado <khanhumayun95@gmail.com>
2022-07-06 01:38:43 +0530

    test(event-display): update unit test for URLOptionsManager

--- packages/phoenix-event-display/src/tests/managers/url-options-manager.test.ts
+++ packages/phoenix-event-display/src/tests/managers/url-options-manager.test.ts
@@ -21,30 +21,7 @@ describe('URLOptionsManager', () => {
     expect(urlOptionsManager).toBeTruthy();
   });
 
-  it('should initialize the event display with event data and configuration from URL', () => {
-    jest.spyOn(urlOptionsManager, 'applyEventOptions');
-    urlOptionsManager.applyEventOptions('defaultEventPath', 'defaultEventType');
-    expect(urlOptionsManager.applyEventOptions).toHaveBeenCalledWith(
-      'defaultEventPath',
-      'defaultEventType'
-    );
-  });
-
-  it('should hide all overlay widgets if "hideWidgets" option from the URL is true', () => {
-    jest.spyOn(urlOptionsManager, 'applyHideWidgetsOptions');
-    urlOptionsManager.applyHideWidgetsOptions();
-    expect(urlOptionsManager.applyHideWidgetsOptions).toHaveBeenCalled();
-  });
-
-  it('should hide all overlay widgets and enable embed menu if "hideWidgets" option from the URL is true', () => {
-    jest.spyOn(urlOptionsManager, 'applyEmbedOption');
-    urlOptionsManager.applyEmbedOption();
-    expect(urlOptionsManager.applyEmbedOption).toHaveBeenCalled();
-  });
-
   it('should get options from URL set through query parameters', () => {
-    jest.spyOn(urlOptionsManager, 'getURLOptions');
-    urlOptionsManager.getURLOptions();
-    expect(urlOptionsManager.getURLOptions).toHaveBeenCalled();
+    expect(urlOptionsManager.getURLOptions()).toBeInstanceOf(URLSearchParams);
   });
 });
