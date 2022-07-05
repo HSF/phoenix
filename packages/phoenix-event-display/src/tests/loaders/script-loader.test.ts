4cc03785937fd611883830b05ba664318ac57420
DamianArado <khanhumayun95@gmail.com>
2022-07-06 00:34:04 +0530

    test(event-display): update unit test for ScriptLoader

--- packages/phoenix-event-display/src/tests/loaders/script-loader.test.ts
+++ packages/phoenix-event-display/src/tests/loaders/script-loader.test.ts
@@ -14,17 +14,24 @@ describe('ScriptLoader', () => {
     expect(scriptLoader).toBeTruthy();
   });
 
+  // using async/await to test the loading of scripts - they take quite long to load
+  // not a good idea to test the script here as it is taking more than 15 seconds to load
+  // right now we only get back a pending promise
+  it('should synchronously load all JSRoot scripts', () => {
+    const JSROOT = ScriptLoader.loadJSRootScripts();
+    expect(JSROOT).toBeInstanceOf(Promise);
+  });
+
   it('should load a script dynamically from a URL', () => {
     const scriptURL = 'https://www.google.com/jsapi';
     const scriptFor = 'google_jsapi';
     const parentElement = document.getElementsByTagName('head')[0];
 
-    jest.spyOn(ScriptLoader, 'loadScript');
-    ScriptLoader.loadScript(scriptURL, scriptFor, parentElement);
-    expect(ScriptLoader.loadScript).toHaveBeenCalledWith(
+    const promise = ScriptLoader.loadScript(
       scriptURL,
       scriptFor,
       parentElement
     );
+    expect(promise).toBeInstanceOf(Promise);
   });
 });
