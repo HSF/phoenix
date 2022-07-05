607012f8a80ae9d14c4a4c8c4fbbd1a5adc8a4d5
DamianArado <khanhumayun95@gmail.com>
2022-07-06 00:09:35 +0530

    test(event-display): update unit test for PhoenixLoader

--- packages/phoenix-event-display/src/tests/loaders/phoenix-loader.test.ts
+++ packages/phoenix-event-display/src/tests/loaders/phoenix-loader.test.ts
@@ -19,70 +19,91 @@ describe('PhoenixLoader', () => {
     expect(phoenixLoader).toBeTruthy();
   });
 
-  it('should take an object that represents ONE event and takes care of adding', () => {
-    const eventData = {};
+  it('should take an object that represents one event and add the different objects to the graphics library and the UI controls', () => {
+    const eventData = [
+      {
+        'event number': 120,
+        'run number': 111,
+        pos: [
+          -2545.135009765625, -2425.1064453125, 7826.09912109375,
+          -2545.135009765625, -1.1222461462020874, 7826.09912109375,
+        ],
+        type: 'Line',
+      },
+    ];
     const graphicsLibrary = new ThreeManager(new InfoLogger());
     const uiManager = new UIManager(new ThreeManager(new InfoLogger()));
     const infoLogger = new InfoLogger();
 
-    jest.spyOn(phoenixLoader, 'buildEventData');
+    jest.spyOn(infoLogger, 'add');
     phoenixLoader.buildEventData(
       eventData,
       graphicsLibrary,
       uiManager,
       infoLogger
     );
-    expect(phoenixLoader.buildEventData).toHaveBeenCalled();
+    expect(infoLogger.add).toBeCalledTimes(1);
   });
 
   it('should get the list of event names from the event data', () => {
     const eventData = {
-      events: [
+      event1: [
         {
-          name: 'event1',
-          start: '2020-01-01T00:00:00.000Z',
-          end: '2020-01-01T00:00:00.000Z',
+          'event number': 120,
+          'run number': 111,
+          pos: [-2545.135009765625, -2425.1064453125, 7826.09912109375],
+          type: 'Line',
         },
+      ],
+      event2: [
         {
-          name: 'event2',
-          start: '2020-01-01T00:00:00.000Z',
-          end: '2020-01-01T00:00:00.000Z',
+          'event number': 121,
+          'run number': 112,
+          pos: [-2545.135009765625, -2425.1064453125, 7826.09912109375],
+          type: 'Line',
         },
       ],
     };
-    jest.spyOn(phoenixLoader, 'getEventsList');
-    phoenixLoader.getEventsList(eventData);
-    expect(phoenixLoader.getEventsList).toHaveBeenCalledWith(eventData);
+    const eventList = phoenixLoader.getEventsList(eventData);
+    expect(eventList).toHaveLength(2);
+    expect(eventList[0]).toBe('event1');
+    expect(eventList[1]).toBe('event2');
   });
 
   it('should get list of collections in the event data', () => {
-    jest.spyOn(phoenixLoader, 'getCollections');
-    phoenixLoader.getCollections();
-    expect(phoenixLoader.getCollections).toHaveBeenCalled();
+    const collectionList = phoenixLoader.getCollections();
+    expect(collectionList).toBe(null);
   });
 
   it('should get the collection with the given collection name from the event data', () => {
-    const collectionName = 'collection1';
-    jest.spyOn(phoenixLoader, 'getCollection');
-    phoenixLoader.getCollection(collectionName);
-    expect(phoenixLoader.getCollection).toHaveBeenCalledWith(collectionName);
+    const collectionName = 'Tracks_V3';
+    const collection = phoenixLoader.getCollection(collectionName);
+    expect(collection).toBeNull;
   });
 
   it('should add label of event object to the labels object', () => {
-    jest.spyOn(phoenixLoader, 'addLabelToEventObject');
-    phoenixLoader.addLabelToEventObject('label', 'collection', 0);
-    expect(phoenixLoader.addLabelToEventObject).toHaveBeenCalled();
+    const label = 'Track #1';
+    const collection = 'Tracks_V3';
+    const indexInCollection = 1;
+    const addedLabel = phoenixLoader.addLabelToEventObject(
+      label,
+      collection,
+      indexInCollection
+    );
+    expect(addedLabel).toBeUndefined;
   });
 
   it('should get the object containing labels of events', () => {
-    jest.spyOn(phoenixLoader, 'getLabelsObject');
-    phoenixLoader.getLabelsObject();
-    expect(phoenixLoader.getLabelsObject).toHaveBeenCalled();
+    const labelsObject = phoenixLoader.getLabelsObject();
+    expect(labelsObject).toBeNull;
   });
 
   it('should get function to add options to scale event object type by given factor', () => {
-    jest.spyOn(phoenixLoader, 'addScaleOptions');
-    phoenixLoader.addScaleOptions('configKey', 'configLabel', () => {});
-    expect(phoenixLoader.addScaleOptions).toHaveBeenCalled();
+    const func = phoenixLoader.addScaleOptions(
+      'configKey',
+      'configLabel',
+      () => {}
+    );
+    expect(func).toBeDefined;
   });
 });
