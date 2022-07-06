49504727a8615f142264ea59bfbfd701005c7e3d
DamianArado <khanhumayun95@gmail.com>
2022-07-06 17:10:12 +0530

    test(event-display): update unit test for DatGUIMenu

--- packages/phoenix-event-display/src/tests/managers/ui-manager/dat-gui-ui.test.ts
+++ packages/phoenix-event-display/src/tests/managers/ui-manager/dat-gui-ui.test.ts
@@ -4,17 +4,21 @@
 import { InfoLogger } from '../../../helpers/info-logger';
 import { ThreeManager } from '../../../managers/three-manager';
 import { DatGUIMenuUI } from '../../../managers/ui-manager/dat-gui-ui';
+import { GUI } from 'dat.gui';
+import { Object3D } from 'three';
 
 jest.mock('../../../managers/three-manager');
 
 describe('DatGUIMenuUI', () => {
   let datGUIMenu: DatGUIMenuUI;
+  let datGUIMenuPrivate: any;
 
   beforeEach(() => {
     datGUIMenu = new DatGUIMenuUI(
       'eventDisplay',
       new ThreeManager(new InfoLogger())
     );
+    datGUIMenuPrivate = datGUIMenu as any;
   });
 
   afterEach(() => {
@@ -26,14 +30,50 @@ describe('DatGUIMenuUI', () => {
   });
 
   it('should add geometry (detector geometry) folder to the menu', () => {
-    jest.spyOn(datGUIMenu, 'addGeometryFolder');
     datGUIMenu.addGeometryFolder();
-    expect(datGUIMenu.addGeometryFolder).toHaveBeenCalled();
+
+    expect(datGUIMenuPrivate.geomFolder).toBeInstanceOf(GUI);
+    expect(datGUIMenuPrivate.geomFolder.name).toBe('Geometries');
+  });
+
+  it('should add geometry to the menu geometry folder and set up its configurable options', () => {
+    datGUIMenu.addGeometryFolder();
+    datGUIMenu.addGeometry(new Object3D(), 'test');
+
+    expect(datGUIMenuPrivate.geomFolder.__folders).toBeInstanceOf(Object);
   });
 
   it('should add event data folder with functions for event data toggles like show/hide and depthTest', () => {
-    jest.spyOn(datGUIMenu, 'addEventDataFolder');
     datGUIMenu.addEventDataFolder();
-    expect(datGUIMenu.addEventDataFolder).toHaveBeenCalled();
+
+    expect(datGUIMenuPrivate.eventFolder).toBeInstanceOf(GUI);
+    expect(datGUIMenuPrivate.eventFolder.name).toBe('Event Data');
+    expect(datGUIMenuPrivate.eventFolder.__folders).toEqual({});
+  });
+
+  it('should add folder for event data type like tracks or hits to the menu', () => {
+    datGUIMenu.addEventDataFolder();
+    datGUIMenu.addEventDataTypeFolder('Event Data');
+
+    expect(
+      datGUIMenuPrivate.eventFolder.__folders['Event Data']
+    ).toBeInstanceOf(GUI);
+  });
+
+  it('should add collection folder and its configurable options to the event data type (tracks, hits etc.) folder', () => {
+    datGUIMenu.addEventDataFolder();
+    datGUIMenu.addCollection('Event Data', 'test');
+
+    expect(
+      datGUIMenuPrivate.eventFolder.__folders['Event Data']
+    ).toBeUndefined();
+  });
+
+  it('should get the folder of the event data type', () => {
+    datGUIMenu.addEventDataFolder();
+    datGUIMenu.addEventDataTypeFolder('Event Data');
+    const eventFolder = datGUIMenu.getEventDataTypeFolder('Event Data');
+
+    expect(eventFolder).toBeInstanceOf(GUI);
   });
 });
