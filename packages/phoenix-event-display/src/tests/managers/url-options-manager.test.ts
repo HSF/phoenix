0983e77ed871269b24b4f6c645eb4827ab422ff8
DamianArado <khanhumayun95@gmail.com>
2022-07-07 02:39:21 +0530

    test(event-display): add more test cases in URLOptionsManager

--- packages/phoenix-event-display/src/tests/managers/url-options-manager.test.ts
+++ packages/phoenix-event-display/src/tests/managers/url-options-manager.test.ts
@@ -9,16 +9,77 @@ jest.mock('../../event-display');
 
 describe('URLOptionsManager', () => {
   let urlOptionsManager: URLOptionsManager;
+  let urlOptionsManagerPrivate: any;
   let eventDisplay: EventDisplay;
   let configuration: Configuration;
 
   beforeEach(() => {
     eventDisplay = new EventDisplay();
+    configuration = {
+      defaultEventFile: {
+        eventFile: 'test file',
+        eventType: 'test event',
+      },
+    };
     urlOptionsManager = new URLOptionsManager(eventDisplay, configuration);
+    urlOptionsManagerPrivate = urlOptionsManager;
   });
 
   it('should create an instance', () => {
     expect(urlOptionsManager).toBeTruthy();
+  });
+
+  it('should initialize and apply all URL options on page load', () => {
+    jest.spyOn(urlOptionsManager, 'applyEventOptions');
+    jest.spyOn(urlOptionsManager, 'applyHideWidgetsOptions');
+    jest.spyOn(urlOptionsManager, 'applyEmbedOption');
+
+    urlOptionsManager.applyOptions();
+
+    expect(urlOptionsManager.applyEventOptions).toHaveBeenCalled();
+    expect(urlOptionsManager.applyHideWidgetsOptions).toHaveBeenCalled();
+    expect(urlOptionsManager.applyEmbedOption).toHaveBeenCalled();
+  });
+
+  it('should hide all overlay widgets if "hideWidgets" option from the URL is true', () => {
+    jest.spyOn(urlOptionsManagerPrivate, 'hideIdsWithURLOption');
+
+    const hideWidgetsOptions = {
+      hideWidgets: [
+        'mainLogo',
+        'uiMenu',
+        'experimentInfo',
+        'phoenixMenu',
+        'statsElement',
+        'gui',
+      ],
+    };
+
+    urlOptionsManager.applyHideWidgetsOptions();
+    expect(urlOptionsManagerPrivate.hideIdsWithURLOption).toHaveBeenCalledWith(
+      hideWidgetsOptions
+    );
+  });
+
+  it('should hide all overlay widgets and enable embed menu if "hideWidgets" option from the URL is true', () => {
+    urlOptionsManagerPrivate.urlOptions.set('embed', 'true');
+    jest.spyOn(urlOptionsManagerPrivate, 'hideIdsWithURLOption');
+
+    const hideWidgetsOptions = {
+      embed: [
+        'mainLogo',
+        'uiMenu',
+        'experimentInfo',
+        'phoenixMenu',
+        'statsElement',
+        'gui',
+      ],
+    };
+
+    urlOptionsManager.applyEmbedOption();
+    expect(urlOptionsManagerPrivate.hideIdsWithURLOption).toHaveBeenCalledWith(
+      hideWidgetsOptions
+    );
   });
 
   it('should get options from URL set through query parameters', () => {
