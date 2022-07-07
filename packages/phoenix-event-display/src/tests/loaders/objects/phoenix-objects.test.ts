f5665c4bd28157410b8135fa99c55cef7585c607
DamianArado <khanhumayun95@gmail.com>
2022-07-07 13:08:39 +0530

    test(event-display): make sure to  keep only relevant assertions in PhoenixObjects

--- packages/phoenix-event-display/src/tests/loaders/objects/phoenix-objects.test.ts
+++ packages/phoenix-event-display/src/tests/loaders/objects/phoenix-objects.test.ts
@@ -1,25 +1,4 @@
-import {
-  BoxGeometry,
-  BufferGeometry,
-  Euler,
-  Float32BufferAttribute,
-  Layers,
-  Line,
-  LineBasicMaterial,
-  LineDashedMaterial,
-  LineSegments,
-  Matrix4,
-  Mesh,
-  MeshPhongMaterial,
-  MeshToonMaterial,
-  Points,
-  PointsMaterial,
-  Quaternion,
-  SphereGeometry,
-  TubeGeometry,
-  Uint16BufferAttribute,
-  Vector3,
-} from 'three';
+import { Line, LineSegments, Mesh, Object3D, Points } from 'three';
 import { PhoenixObjects } from '../../../loaders/objects/phoenix-objects';
 
 describe('PhoenixObjects', () => {
@@ -54,39 +33,9 @@ describe('PhoenixObjects', () => {
 
     expect(trackObject.children.length).toBe(2);
     expect(trackObject.children[0].type).toBe('Mesh');
+    expect(trackObject.children[0].name).toBe('Track');
     expect(trackObject.children[1].type).toBe('Line');
-
-    const trackMesh = trackObject.children[0] as Mesh;
-    expect(trackMesh.children.length).toBe(0);
-    expect(trackMesh.position).toBeInstanceOf(Vector3);
-    expect(trackMesh.rotation).toBeInstanceOf(Euler);
-    expect(trackMesh.matrix).toBeInstanceOf(Matrix4);
-    expect(trackMesh.matrixAutoUpdate).toBe(true);
-    expect(trackMesh.layers).toBeInstanceOf(Layers);
-    expect(trackMesh.visible).toBe(true);
-    expect(trackMesh.castShadow).toBe(false);
-    expect(trackMesh.receiveShadow).toBe(false);
-    expect(trackMesh.frustumCulled).toBe(true);
-    expect(trackMesh.renderOrder).toBe(0);
-    expect(trackMesh.userData).toBeInstanceOf(Object);
-    expect(trackMesh.geometry).toBeInstanceOf(TubeGeometry);
-    expect(trackMesh.material).toBeInstanceOf(MeshToonMaterial);
-
-    const trackLine = trackObject.children[1] as Line;
-    expect(trackLine.children.length).toBe(0);
-    expect(trackLine.position).toBeInstanceOf(Vector3);
-    expect(trackLine.rotation).toBeInstanceOf(Euler);
-    expect(trackLine.matrix).toBeInstanceOf(Matrix4);
-    expect(trackLine.matrixAutoUpdate).toBe(true);
-    expect(trackLine.layers).toBeInstanceOf(Layers);
-    expect(trackLine.visible).toBe(true);
-    expect(trackLine.castShadow).toBe(false);
-    expect(trackLine.receiveShadow).toBe(false);
-    expect(trackLine.frustumCulled).toBe(true);
-    expect(trackLine.renderOrder).toBe(0);
-    expect(trackLine.userData).toBeInstanceOf(Object);
-    expect(trackLine.geometry).toBeInstanceOf(BufferGeometry);
-    expect(trackLine.material).toBeInstanceOf(LineBasicMaterial);
+    expect(trackObject.children[1].name).toBe('Track');
 
     expect(trackObject.userData.pos).toEqual(trackParams.pos);
     expect(trackObject.userData.dparams).toEqual(trackParams.dparams);
@@ -113,19 +62,7 @@ describe('PhoenixObjects', () => {
     const jetObject = PhoenixObjects.getJet(jetParams) as Mesh;
 
     expect(jetObject.name).toBe('Jet');
-    expect(jetObject.children.length).toBe(0);
-    expect(jetObject.up).toBeInstanceOf(Vector3);
-    expect(jetObject.position).toBeInstanceOf(Vector3);
-    expect(jetObject.rotation).toBeInstanceOf(Euler);
-    expect(jetObject.quaternion).toBeInstanceOf(Quaternion);
-    expect(jetObject.matrix).toBeInstanceOf(Matrix4);
-    expect(jetObject.matrixAutoUpdate).toBe(true);
-    expect(jetObject.layers).toBeInstanceOf(Layers);
-    expect(jetObject.visible).toBe(true);
-    expect(jetObject.castShadow).toBe(false);
-    expect(jetObject.receiveShadow).toBe(false);
-    expect(jetObject.frustumCulled).toBe(true);
-    expect(jetObject.renderOrder).toBe(0);
+    expect(jetObject.type).toBe('Mesh');
 
     expect(jetObject.userData.eta).toBe(jetParams.eta);
     expect(jetObject.userData.phi).toBe(jetParams.phi);
@@ -134,17 +71,9 @@ describe('PhoenixObjects', () => {
     expect(jetObject.userData.et).toBe(jetParams.et);
     expect(jetObject.userData.coneR).toBe(jetParams.coneR);
     expect(jetObject.userData.color).toBe(jetParams.color);
-
-    expect(jetObject.geometry.type).toBe('CylinderGeometry');
-    expect(jetObject.geometry.index).toBeInstanceOf(Uint16BufferAttribute);
-    expect(jetObject.geometry.attributes.position).toBeInstanceOf(
-      Float32BufferAttribute
-    );
-    expect(jetObject.geometry.drawRange.start).toBe(0);
-    expect(jetObject.geometry.drawRange.count).toBe(Infinity);
   });
 
-  it('should create Hits from the given parameters and get it as an object', () => {
+  it('should create Hits of Line type from the given parameters and get it as an object', () => {
     const hitsParamsLine = [
       {
         pos: [
@@ -155,12 +84,12 @@ describe('PhoenixObjects', () => {
       },
     ];
     const hitsObjectLine = PhoenixObjects.getHits(hitsParamsLine) as Line;
+
     expect(hitsObjectLine).toBeInstanceOf(LineSegments);
     expect(hitsObjectLine.name).toBe('LineHit');
-    expect(hitsObjectLine.children.length).toBe(0);
-    expect(hitsObjectLine.geometry.type).toBe('BufferGeometry');
-    expect(hitsObjectLine.material).toBeInstanceOf(LineBasicMaterial);
+  });
 
+  it('should create Hits of Point type from the given parameters and get it as an object', () => {
     const hitsParamsPoint = [
       {
         pos: [
@@ -171,12 +100,12 @@ describe('PhoenixObjects', () => {
       },
     ];
     const hitsObjectPoint = PhoenixObjects.getHits(hitsParamsPoint) as Points;
+
     expect(hitsObjectPoint.type).toBe('Points');
     expect(hitsObjectPoint.name).toBe('Hit');
-    expect(hitsObjectPoint.children.length).toBe(0);
-    expect(hitsObjectPoint.geometry.type).toBe('BufferGeometry');
-    expect(hitsObjectPoint.material).toBeInstanceOf(PointsMaterial);
+  });
 
+  it('should create Hits of Box type from the given parameters and get it as an object', () => {
     const hitsParamsBox = [
       {
         pos: [
@@ -188,11 +117,11 @@ describe('PhoenixObjects', () => {
     ];
     const hitsObjectBox = PhoenixObjects.getHits(hitsParamsBox) as Mesh;
 
+    expect(hitsObjectBox.type).toBe('Mesh');
     expect(hitsObjectBox.name).toBe('BoxHit');
-    expect(hitsObjectBox.children.length).toBe(0);
-    expect(hitsObjectBox.geometry.type).toBe('BufferGeometry');
-    expect(hitsObjectBox.material).toBeInstanceOf(MeshPhongMaterial);
+  });
 
+  it('should create Hits of Unknown type from the given parameters and get it as an object', () => {
     const unknownHitsParams = [
       {
         pos: [4846, 23, 18, 126, 1565, 35404],
@@ -200,49 +129,15 @@ describe('PhoenixObjects', () => {
       },
     ];
     const unknownHitsObject = PhoenixObjects.getHits(unknownHitsParams);
-    expect(unknownHitsObject).toBe(undefined);
+    expect(unknownHitsObject).toBeUndefined();
   });
 
   it('should create a Cluster and get it as an object', () => {
     const clusterParams = {};
     const clusterObject = PhoenixObjects.getCluster(clusterParams) as Mesh;
 
+    expect(clusterObject.name).toBe('Cluster');
     expect(clusterObject.type).toBe('Mesh');
-    expect(clusterObject.children.length).toBe(0);
-    expect(clusterObject.up).toBeInstanceOf(Vector3);
-
-    expect(clusterObject.position).toBeInstanceOf(Vector3);
-    expect(clusterObject.position.x).toBe(NaN);
-    expect(clusterObject.position.y).toBe(NaN);
-    expect(clusterObject.position.z).toBe(NaN);
-
-    expect(clusterObject.rotation).toBeInstanceOf(Euler);
-    expect(clusterObject.rotation.x).toBe(NaN);
-    expect(clusterObject.rotation.y).toBe(NaN);
-    expect(clusterObject.rotation.z).toBe(0);
-    expect(clusterObject.rotation.order).toBe('XYZ');
-
-    expect(clusterObject.quaternion).toBeInstanceOf(Quaternion);
-    expect(clusterObject.quaternion.x).toBe(NaN);
-    expect(clusterObject.quaternion.y).toBe(NaN);
-    expect(clusterObject.quaternion.z).toBe(NaN);
-    expect(clusterObject.quaternion.w).toBe(NaN);
-
-    expect(clusterObject.scale).toBeInstanceOf(Vector3);
-    expect(clusterObject.scale.x).toBe(1);
-    expect(clusterObject.scale.y).toBe(1);
-    expect(clusterObject.scale.z).toBe(1);
-
-    expect(clusterObject.matrix).toBeInstanceOf(Matrix4);
-    expect(clusterObject.matrixAutoUpdate).toBe(true);
-    expect(clusterObject.layers).toBeInstanceOf(Layers);
-    expect(clusterObject.visible).toBe(true);
-    expect(clusterObject.castShadow).toBe(false);
-    expect(clusterObject.receiveShadow).toBe(false);
-    expect(clusterObject.frustumCulled).toBe(true);
-    expect(clusterObject.renderOrder).toBe(0);
-    expect(clusterObject.geometry).toBeInstanceOf(BoxGeometry);
-    expect(clusterObject.material).toBeInstanceOf(MeshPhongMaterial);
   });
 
   it('should create a Calocell from the given parameters and get it as an object', () => {
@@ -257,41 +152,13 @@ describe('PhoenixObjects', () => {
     const caloCellObject = PhoenixObjects.getCaloCell(caloCells) as Mesh;
 
     expect(caloCellObject.name).toBe('Cluster');
-    expect(caloCellObject.position).toBeInstanceOf(Vector3);
-
-    expect(caloCellObject.rotation).toBeInstanceOf(Euler);
-    expect(caloCellObject.rotation.x).toBe(0);
-    expect(caloCellObject.rotation.y).toBe(0);
-    expect(caloCellObject.rotation.z).toBe(0);
-    expect(caloCellObject.rotation.order).toBe('XYZ');
-
-    expect(caloCellObject.quaternion).toBeInstanceOf(Quaternion);
-    expect(caloCellObject.quaternion.x).toBe(0);
-    expect(caloCellObject.quaternion.y).toBe(0);
-    expect(caloCellObject.quaternion.z).toBe(0);
-    expect(caloCellObject.quaternion.w).toBe(1);
-
-    expect(caloCellObject.scale).toBeInstanceOf(Vector3);
-    expect(caloCellObject.scale.x).toBe(1);
-    expect(caloCellObject.scale.y).toBe(1);
-    expect(caloCellObject.scale.z).toBe(1);
-
-    expect(caloCellObject.matrix).toBeInstanceOf(Matrix4);
-    expect(caloCellObject.matrixAutoUpdate).toBe(true);
-    expect(caloCellObject.layers).toBeInstanceOf(Layers);
-    expect(caloCellObject.visible).toBe(true);
-    expect(caloCellObject.castShadow).toBe(false);
-    expect(caloCellObject.receiveShadow).toBe(false);
-    expect(caloCellObject.frustumCulled).toBe(true);
-    expect(caloCellObject.renderOrder).toBe(0);
+    expect(caloCellObject.type).toBe('Mesh');
 
     expect(caloCellObject.userData.energy).toBe(caloCells.energy);
     expect(caloCellObject.userData.phi).toBe(caloCells.phi);
     expect(caloCellObject.userData.eta).toBe(caloCells.eta);
     expect(caloCellObject.userData.theta).toBe(caloCells.theta);
     expect(caloCellObject.userData.uuid).not.toBe(caloCells.uuid);
-
-    expect(caloCellObject.geometry.type).toBe('BoxGeometry');
   });
 
   it('should create a PlanarCaloCell from the given parameters and get it as an object', () => {
@@ -313,36 +180,9 @@ describe('PhoenixObjects', () => {
     const planarCaloCellObject =
       PhoenixObjects.getPlanarCaloCell(planarCaloCellParams);
 
+    expect(planarCaloCellObject).toBeInstanceOf(Object3D);
+    expect(planarCaloCellObject.name).toBe('PlanarCaloCell');
     expect(planarCaloCellObject.type).toBe('Object3D');
-    expect(planarCaloCellObject.children.length).toBe(1);
-
-    const caloCell = planarCaloCellObject.children[0];
-    expect(caloCell).toBeInstanceOf(Mesh);
-    expect(caloCell.up).toBeInstanceOf(Vector3);
-    expect(caloCell.position).toBeInstanceOf(Vector3);
-    expect(caloCell.rotation).toBeInstanceOf(Euler);
-    expect(caloCell.quaternion).toBeInstanceOf(Quaternion);
-    expect(caloCell.scale).toBeInstanceOf(Vector3);
-    expect(caloCell.matrix).toBeInstanceOf(Matrix4);
-    expect(caloCell.matrixAutoUpdate).toBe(true);
-    expect(caloCell.layers).toBeInstanceOf(Layers);
-    expect(caloCell.visible).toBe(true);
-    expect(caloCell.castShadow).toBe(false);
-    expect(caloCell.receiveShadow).toBe(false);
-    expect(caloCell.frustumCulled).toBe(true);
-    expect(caloCell.renderOrder).toBe(0);
-
-    expect(planarCaloCellObject.up).toBeInstanceOf(Vector3);
-    expect(planarCaloCellObject.up.x).toBe(0);
-    expect(planarCaloCellObject.up.y).toBe(1);
-    expect(planarCaloCellObject.up.z).toBe(0);
-
-    expect(planarCaloCellObject.position).toBeInstanceOf(Vector3);
-    expect(planarCaloCellObject.position.x).toBe(0);
-    expect(planarCaloCellObject.position.y).toBe(0);
-    expect(planarCaloCellObject.position.z).toBe(0);
-
-    expect(planarCaloCellObject.rotation).toBeInstanceOf(Euler);
 
     expect(planarCaloCellObject.userData.energy).toBe(
       planarCaloCellParams.energy
@@ -375,31 +215,7 @@ describe('PhoenixObjects', () => {
     const vertexObject = PhoenixObjects.getVertex(vertexParams) as Mesh;
 
     expect(vertexObject.name).toBe('Vertex');
-    expect(vertexObject.children.length).toBe(0);
-
-    expect(vertexObject.up).toBeInstanceOf(Vector3);
-    expect(vertexObject.up.x).toBe(0);
-    expect(vertexObject.up.y).toBe(1);
-    expect(vertexObject.up.z).toBe(0);
-
-    expect(vertexObject.position).toBeInstanceOf(Vector3);
-    expect(vertexObject.position.x).toBe(vertexParams.x);
-    expect(vertexObject.position.y).toBe(vertexParams.y);
-    expect(vertexObject.position.z).toBe(vertexParams.z);
-
-    expect(vertexObject.rotation).toBeInstanceOf(Euler);
-    expect(vertexObject.rotation.x).toBe(0);
-    expect(vertexObject.rotation.y).toBe(0);
-    expect(vertexObject.rotation.z).toBe(0);
-    expect(vertexObject.rotation.order).toBe('XYZ');
-
-    expect(vertexObject.quaternion).toBeInstanceOf(Quaternion);
-    expect(vertexObject.matrix).toBeInstanceOf(Matrix4);
-    expect(vertexObject.matrixAutoUpdate).toBe(true);
-    expect(vertexObject.layers).toBeInstanceOf(Layers);
-
-    expect(vertexObject.geometry).toBeInstanceOf(SphereGeometry);
-    expect(vertexObject.material).toBeInstanceOf(MeshPhongMaterial);
+    expect(vertexObject.type).toBe('Mesh');
   });
 
   it('should create a Vertex from the given parameters and get it as a MET object', () => {
@@ -413,25 +229,8 @@ describe('PhoenixObjects', () => {
     const metObject = PhoenixObjects.getMissingEnergy(metParams) as Line;
 
     expect(metObject.name).toBe('Missing Energy');
-    expect(metObject.children.length).toBe(0);
+    expect(metObject.type).toBe('Line');
 
-    expect(metObject.up).toBeInstanceOf(Vector3);
-    expect(metObject.up.x).toBe(0);
-    expect(metObject.up.y).toBe(1);
-    expect(metObject.up.z).toBe(0);
-
-    expect(metObject.position).toBeInstanceOf(Vector3);
-    expect(metObject.position.x).toBe(0);
-    expect(metObject.position.y).toBe(0);
-    expect(metObject.position.z).toBe(0);
-
-    expect(metObject.rotation).toBeInstanceOf(Euler);
-    expect(metObject.rotation.x).toBe(0);
-    expect(metObject.rotation.y).toBe(0);
-    expect(metObject.rotation.z).toBe(0);
-
-    expect(metObject.geometry).toBeInstanceOf(BufferGeometry);
-    expect(metObject.material).toBeInstanceOf(LineDashedMaterial);
     expect(metObject.userData.color).toBe(metParams.color);
     expect(metObject.userData.etx).toBe(metParams.etx);
     expect(metObject.userData.ety).toBe(metParams.ety);
