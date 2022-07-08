8c601247dce0be8b3e7c58547a2a751746a3c09b
DamianArado <khanhumayun95@gmail.com>
2022-07-09 02:17:37 +0530

    test(event-display): update test cases for PhoenixLoader

--- packages/phoenix-event-display/src/tests/loaders/phoenix-loader.test.ts
+++ packages/phoenix-event-display/src/tests/loaders/phoenix-loader.test.ts
@@ -10,91 +10,108 @@ jest.mock('../../managers/three-manager/index');
 
 describe('PhoenixLoader', () => {
   let phoenixLoader: PhoenixLoader;
+  let eventData: any;
 
   beforeEach(() => {
     phoenixLoader = new PhoenixLoader();
-  });
 
-  it('should create an instance', () => {
-    expect(phoenixLoader).toBeTruthy();
-  });
-
-  it('should take an object that represents one event and add the different objects to the graphics library and the UI controls', () => {
-    const eventData = [
-      {
-        'event number': 120,
-        'run number': 111,
-        pos: [
-          -2545.135009765625, -2425.1064453125, 7826.09912109375,
-          -2545.135009765625, -1.1222461462020874, 7826.09912109375,
-        ],
-        type: 'Line',
+    eventData = {
+      Event: {
+        'event number': 1,
+        'run number': 1,
+        Hits: {
+          hitsCollection: [
+            {
+              pos: [
+                -2545.135009765625, -2425.1064453125, 7826.09912109375,
+                -2545.135009765625, -1.1222461462020874, 7826.09912109375,
+              ],
+              type: 'Line',
+            },
+          ],
+        },
       },
-    ];
+      Event2: {
+        'event number': 2,
+        'run number': 2,
+        Hits: {
+          hitsCollection: [
+            {
+              pos: [
+                -2545.135009765625, -2425.1064453125, 7826.09912109375,
+                -2545.135009765625, -1.1222461462020874, 7826.09912109375,
+              ],
+              type: 'Line',
+            },
+          ],
+        },
+      },
+    };
+
     const graphicsLibrary = new ThreeManager(new InfoLogger());
     const uiManager = new UIManager(new ThreeManager(new InfoLogger()));
     const infoLogger = new InfoLogger();
 
-    jest.spyOn(infoLogger, 'add');
     phoenixLoader.buildEventData(
       eventData,
       graphicsLibrary,
       uiManager,
       infoLogger
     );
-    expect(infoLogger.add).toBeCalledTimes(1);
+  });
+
+  it('should create an instance', () => {
+    expect(phoenixLoader).toBeTruthy();
   });
 
   it('should get the list of event names from the event data', () => {
-    const eventData = {
-      event1: [
-        {
-          'event number': 120,
-          'run number': 111,
-          pos: [-2545.135009765625, -2425.1064453125, 7826.09912109375],
-          type: 'Line',
-        },
-      ],
-      event2: [
-        {
-          'event number': 121,
-          'run number': 112,
-          pos: [-2545.135009765625, -2425.1064453125, 7826.09912109375],
-          type: 'Line',
-        },
-      ],
-    };
-    const eventList = phoenixLoader.getEventsList(eventData);
-    expect(eventList).toHaveLength(2);
-    expect(eventList[0]).toBe('event1');
-    expect(eventList[1]).toBe('event2');
+    const eventsList = phoenixLoader.getEventsList(eventData);
+
+    expect(eventsList).toHaveLength(2);
+    expect(eventsList[0]).toBe('Event');
+    expect(eventsList[1]).toBe('Event2');
   });
 
   it('should get list of collections in the event data', () => {
     const collectionList = phoenixLoader.getCollections();
-    expect(collectionList).toBe(null);
+
+    expect(collectionList).toEqual([
+      'event number',
+      'run number',
+      'Hits',
+      'event number',
+      'run number',
+      'Hits',
+    ]);
   });
 
   it('should get the collection with the given collection name from the event data', () => {
-    const collectionName = 'Tracks_V3';
-    const collection = phoenixLoader.getCollection(collectionName);
-    expect(collection).toBeNull;
+    const collectionName = {
+      hitsCollection: [
+        {
+          pos: [
+            -2545.135009765625, -2425.1064453125, 7826.09912109375,
+            -2545.135009765625, -1.1222461462020874, 7826.09912109375,
+          ],
+          type: 'Line',
+        },
+      ],
+    };
+
+    const collection = phoenixLoader.getCollection('Hits');
+
+    expect(collection).toEqual(collectionName);
   });
 
-  it('should add label of event object to the labels object', () => {
-    const label = 'Track #1';
-    const collection = 'Tracks_V3';
-    const indexInCollection = 1;
-    const addedLabel = phoenixLoader.addLabelToEventObject(
-      label,
-      collection,
-      indexInCollection
-    );
-    expect(addedLabel).toBeUndefined;
+  it('get metadata associated to any event', () => {
+    const metadata = phoenixLoader.getEventMetadata();
+
+    expect(metadata).toBeDefined();
   });
 
   it('should get the object containing labels of events', () => {
     const labelsObject = phoenixLoader.getLabelsObject();
+    // as we didnt add labels for any
     expect(labelsObject).toBeNull;
   });
 
@@ -104,6 +121,7 @@ describe('PhoenixLoader', () => {
       'configLabel',
       () => {}
     );
+
     expect(func).toBeDefined;
   });
 });
