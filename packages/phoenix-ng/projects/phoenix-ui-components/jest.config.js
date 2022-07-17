681e9be97954755fe29a13b7c28dc68357ab498d
DamianArado <khanhumayun95@gmail.com>
2022-07-18 02:02:37 +0530

    test(app): add initial configuration to run unit tests using Jest

---
+++ packages/phoenix-ng/projects/phoenix-ui-components/jest.config.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { pathsToModuleNameMapper } = require('ts-jest');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { paths } = require('./tsconfig.json').compilerOptions;
+
+// eslint-disable-next-line no-undef
+globalThis.ngJest = {
+  skipNgcc: false,
+  tsconfig: 'tsconfig.spec.json',
+};
+
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'jest-preset-angular',
+  globalSetup: 'jest-preset-angular/global-setup',
+  moduleNameMapper: pathsToModuleNameMapper(paths, { prefix: '<rootDir>' }),
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+};
