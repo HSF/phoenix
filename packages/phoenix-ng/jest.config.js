f58e1645276d12fa95e0cf726ce5b5f16ba2616e
DamianArado <khanhumayun95@gmail.com>
2022-07-24 20:53:16 +0530

    test(app): changed jest config but the tests wont run

--- packages/phoenix-ng/jest.config.js
+++ packages/phoenix-ng/jest.config.js
@@ -2,6 +2,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { pathsToModuleNameMapper } = require('ts-jest');
 const { paths } = require('./tsconfig.json').compilerOptions;
+const esModules = ['@angular', '@ngrx'];
 
 // eslint-disable-next-line no-undef
 globalThis.ngJest = {
@@ -11,8 +12,20 @@ globalThis.ngJest = {
 
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
+  roots: ['projects'],
   preset: 'jest-preset-angular',
   globalSetup: 'jest-preset-angular/global-setup',
   moduleNameMapper: pathsToModuleNameMapper(paths, { prefix: '<rootDir>' }),
+  moduleFileExtensions: ['ts', 'html', 'js', 'json', 'mjs'],
+  transform: {
+    '^.+\\.(ts|js|mjs|html|svg)$': 'jest-preset-angular',
+  },
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  transformIgnorePatterns: [
+    '/node_modules/(?!three/examples/jsm/.+\\.js)',
+    `<rootDir>/node_modules/(?!.*\\.mjs$|${esModules.join('|')})`,
+    'node_modules/(?!.*\\.mjs$)',
+  ],
+  verbose: true,
+  collectCoverageFrom: ['<rootDir>/projects/**/*.ts'],
 };
