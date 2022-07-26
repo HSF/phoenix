dfb4177c818403fac41835ee440721dea15d93be
DamianArado <khanhumayun95@gmail.com>
2022-07-27 01:37:23 +0530

    Revert "test(app): remove Jasmine-Karma setup and add Jest config"
    This reverts commit b80a7c304e2fcabda94f82906cf310b5dd29279a.

---
+++ packages/phoenix-ng/projects/phoenix-app/karma.conf.js
@@ -0,0 +1,43 @@
+/* eslint-disable */
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    // Timeout for browser disconnection error.
+    pingTimeout: 15000,
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma'),
+    ],
+    client: {
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+      jasmine: {
+        random: false,
+      },
+    },
+    reporters: ['progress', 'coverage'],
+    coverageReporter: {
+      dir: require('path').join(__dirname, '../../coverage/phoenix-app'),
+      subdir: '.',
+      reporters: [{ type: 'lcov' }, { type: 'text-summary' }],
+    },
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox'],
+      },
+    },
+    singleRun: false,
+    restartOnFileChange: true,
+  });
+};
