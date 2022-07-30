a48e95601e48991b91835201286589ba742eafec
DamianArado <khanhumayun95@gmail.com>
2022-07-30 23:27:39 +0530

    minor change - fix linting errors

--- packages/phoenix-ng/jest.config.js
+++ packages/phoenix-ng/jest.config.js
@@ -21,7 +21,7 @@ module.exports = {
   preset: 'jest-preset-angular',
   resolver: '@nrwl/jest/plugins/resolver',
   globalSetup: 'jest-preset-angular/global-setup',
-  testEnvironment: "jsdom",
+  testEnvironment: 'jsdom',
   moduleNameMapper: pathsToModuleNameMapper(paths, { prefix: '<rootDir>' }),
   moduleFileExtensions: ['ts', 'html', 'js', 'json', 'mjs'],
   transform: {
