13261acfce8b1b02db6b285f2a83b48aa14fdb51
DamianArado <khanhumayun95@gmail.com>
2022-07-31 03:14:46 +0530

    test(app): update unit tests for InfoPanelOverlayComponent using Jest syntax

--- packages/phoenix-ng/projects/phoenix-ui-components/lib/components/ui-menu/info-panel/info-panel-overlay/info-panel-overlay.component.spec.ts
+++ packages/phoenix-ng/projects/phoenix-ui-components/lib/components/ui-menu/info-panel/info-panel-overlay/info-panel-overlay.component.spec.ts
@@ -1,15 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { InfoPanelOverlayComponent } from './info-panel-overlay.component';
-import { PhoenixUIModule } from '../../../phoenix-ui.module';
+import { EventDisplayService } from 'phoenix-ui-components';
 
 describe('InfoPanelOverlayComponent', () => {
   let component: InfoPanelOverlayComponent;
   let fixture: ComponentFixture<InfoPanelOverlayComponent>;
 
+  const mockEventDisplay = {
+    getInfoLogger: jest.fn().mockReturnThis(),
+    getInfoLoggerList: jest.fn().mockReturnThis(),
+  };
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [PhoenixUIModule],
+      providers: [
+        {
+          provide: EventDisplayService,
+          useValue: mockEventDisplay,
+        },
+      ],
       declarations: [InfoPanelOverlayComponent],
     }).compileComponents();
   });
@@ -17,10 +27,19 @@ describe('InfoPanelOverlayComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(InfoPanelOverlayComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    //fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call getInfoLoggerList', () => {
+    component.ngOnInit();
+
+    expect(
+      mockEventDisplay.getInfoLogger().getInfoLoggerList
+    ).toHaveBeenCalled();
+    expect(component.actionsList).toBeTruthy();
   });
 });
