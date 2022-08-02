fe2dfd1dc16e6a79b4a1d8b71801d7a103035d92
DamianArado <khanhumayun95@gmail.com>
2022-08-02 18:50:06 +0530

    test(app): fix unit test for HomeComponent

--- packages/phoenix-ng/projects/phoenix-app/src/app/home/home.component.spec.ts
+++ packages/phoenix-ng/projects/phoenix-app/src/app/home/home.component.spec.ts
@@ -1,14 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { EventDisplayService } from '../../../../phoenix-ui-components/lib/services/event-display.service';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
+  const mockEventDisplay = {
+    getThreeManager: jest.fn().mockReturnThis(),
+    stopAnimationLoop: jest.fn().mockReturnThis(),
+    getUIManager: jest.fn().mockReturnThis(),
+    detectColorScheme: jest.fn().mockReturnThis(),
+  };
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [HomeComponent],
+      providers: [
+        {
+          provide: EventDisplayService,
+          useValue: mockEventDisplay,
+        },
+      ],
     }).compileComponents();
   });
 
