3e1930d9ede38bb103632e0a1c078e802d7d901a
DamianArado <khanhumayun95@gmail.com>
2022-08-02 18:00:47 +0530

    test(app): fix unit tests for ObjectSelectionOverlayComponent

--- packages/phoenix-ng/projects/phoenix-ui-components/lib/components/ui-menu/object-selection/object-selection-overlay/object-selection-overlay.component.spec.ts
+++ packages/phoenix-ng/projects/phoenix-ui-components/lib/components/ui-menu/object-selection/object-selection-overlay/object-selection-overlay.component.spec.ts
@@ -2,14 +2,25 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ObjectSelectionOverlayComponent } from './object-selection-overlay.component';
 import { PhoenixUIModule } from '../../../phoenix-ui.module';
+import { EventDisplayService } from 'phoenix-ui-components';
 
 describe('ObjectSelectionOverlayComponent', () => {
   let component: ObjectSelectionOverlayComponent;
   let fixture: ComponentFixture<ObjectSelectionOverlayComponent>;
 
+  const mockEventDisplay = {
+    allowSelection: jest.fn(),
+  };
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [PhoenixUIModule],
+      providers: [
+        {
+          provide: EventDisplayService,
+          useValue: mockEventDisplay,
+        },
+      ],
     }).compileComponents();
   });
 
