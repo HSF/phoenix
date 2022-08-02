c52093a9e75b2a10e0dd1dc7f8b388b4b628d8bb
DamianArado <khanhumayun95@gmail.com>
2022-08-02 12:36:38 +0530

    test(app): fix unit tests for ArToggleComponent

--- packages/phoenix-ng/projects/phoenix-ui-components/lib/components/ui-menu/ar-toggle/ar-toggle.component.spec.ts
+++ packages/phoenix-ng/projects/phoenix-ui-components/lib/components/ui-menu/ar-toggle/ar-toggle.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { EventDisplayService } from 'phoenix-ui-components';
 
 import { ArToggleComponent } from './ar-toggle.component';
 
@@ -6,8 +7,19 @@ describe('ArToggleComponent', () => {
   let component: ArToggleComponent;
   let fixture: ComponentFixture<ArToggleComponent>;
 
+  const mockEventDisplay = {
+    initXR: jest.fn(),
+    endXR: jest.fn(),
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: EventDisplayService,
+          useValue: mockEventDisplay,
+        },
+      ],
       declarations: [ArToggleComponent],
     }).compileComponents();
   });
@@ -20,5 +32,15 @@ describe('ArToggleComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should toggle AR from OFF to ON and vice-versa', () => {
+    expect(component.arActive).toBeFalsy();
+
+    component.toggleAr(true);
+    expect(component.arActive).toBeTruthy();
+
+    component.toggleAr(false);
+    expect(component.arActive).toBeFalsy();
   });
 });
