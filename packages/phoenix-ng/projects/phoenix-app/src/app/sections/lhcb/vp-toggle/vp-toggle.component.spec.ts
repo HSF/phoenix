19419ff97b8fb17c82b940da3fdf609e8ec490b3
DamianArado <khanhumayun95@gmail.com>
2022-08-03 14:51:58 +0530

    test(app): fix unit test for VPToggleComponent

--- packages/phoenix-ng/projects/phoenix-app/src/app/sections/lhcb/vp-toggle/vp-toggle.component.spec.ts
+++ packages/phoenix-ng/projects/phoenix-app/src/app/sections/lhcb/vp-toggle/vp-toggle.component.spec.ts
@@ -1,13 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { EventDisplayService } from 'phoenix-ui-components';
 
 import { VPToggleComponent } from './vp-toggle.component';
+import { SceneManager } from '../../../../../../../../phoenix-event-display/src/managers/three-manager/scene-manager';
 
 describe('VPToggleComponent', () => {
   let component: VPToggleComponent;
   let fixture: ComponentFixture<VPToggleComponent>;
 
+  const mockEventDisplay = {
+    getThreeManager: jest.fn().mockReturnThis(),
+    getSceneManager: jest.fn().mockReturnThis(),
+  };
+
+  const sceneManager = mockEventDisplay.getThreeManager().getSceneManager();
+  (sceneManager as any).getObjectByName = jest.fn().mockReturnValue({
+    position: {
+      setComponent: jest.fn(),
+    },
+  } as any);
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: EventDisplayService,
+          useValue: mockEventDisplay,
+        },
+      ],
       declarations: [VPToggleComponent],
     }).compileComponents();
   });
@@ -23,8 +43,8 @@ describe('VPToggleComponent', () => {
   });
 
   it('should toggle VP opening/closing', () => {
-    expect(component.open).toBe(false);
+    expect(component.open).toBeFalsy();
     component.toggleVP();
-    expect(component.open).toBe(true);
+    expect(component.open).toBeTruthy();
   });
 });
