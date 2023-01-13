b03870dab9ebb4bad42004eb8f74097a3062c2b9
Edward Moyse <edward.moyse@gmail.com>
2023-01-13 13:38:23 +0100

    add toggleMinMaxCut methods

--- packages/phoenix-ng/projects/phoenix-ui-components/lib/components/phoenix-menu/config/config-slider/config-slider.component.ts
+++ packages/phoenix-ng/projects/phoenix-ui-components/lib/components/phoenix-menu/config/config-slider/config-slider.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { MatCheckboxChange } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-config-slider',
@@ -8,7 +9,9 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 export class ConfigSliderComponent {
   @Input() value: number = 0;
   @Input() min: number = 0;
+  oldMin: number = 0;
   @Input() max: number = 100;
+  oldMax: number = 0;
   @Input() step: number = 1;
   @Input() allowCustomValue: boolean = false;
   @Output() onChange: EventEmitter<number> = new EventEmitter<number>();
@@ -17,5 +20,27 @@ export class ConfigSliderComponent {
     if (value) {
       this.onChange.emit(value);
     }
+  }
+
+  toggleMinCut(change: MatCheckboxChange) {
+    const value = change.checked;
+    if (value) {
+      this.min = this.oldMin;
+    } else {
+      this.oldMin = this.min;
+      this.min = Number.MIN_SAFE_INTEGER;
+    }
+    // We should probably disable the min input too, and change text to be infinity symbol or something?
+  }
+
+  toggleMaxCut(change: MatCheckboxChange) {
+    const value = change.checked;
+    if (value) {
+      this.max = this.oldMax;
+    } else {
+      this.oldMax = this.max;
+      this.max = Number.MIN_SAFE_INTEGER;
+    }
+    // We should probably disable the min input too, and change text to be infinity symbol or something?
   }
 }
