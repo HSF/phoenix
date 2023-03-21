303a67cf80536f0f935f760cfa6bb3e141e1517f
Fawad Ali <m.fawaadali98@gmail.com>
2023-03-22 00:17:50 +0100

    Fix min and max cut checkboxes not working

--- packages/phoenix-ng/projects/phoenix-ui-components/lib/components/phoenix-menu/config/config-slider/config-slider.component.ts
+++ packages/phoenix-ng/projects/phoenix-ui-components/lib/components/phoenix-menu/config/config-slider/config-slider.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { MatCheckboxChange } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-config-slider',
@@ -9,36 +8,12 @@ import { MatCheckboxChange } from '@angular/material/checkbox';
 export class ConfigSliderComponent {
   @Input() value: number = 0;
   @Input() min: number = 0;
-  oldMin: number = 0;
   @Input() max: number = 100;
-  oldMax: number = 0;
   @Input() step: number = 1;
   @Input() allowCustomValue: boolean = false;
   @Output() onChange: EventEmitter<number> = new EventEmitter<number>();
 
   onValueChange(value: number) {
     value && this.onChange.emit(value);
-  }
-
-  toggleMinCut(change: MatCheckboxChange) {
-    const value = change.checked;
-    if (value) {
-      this.min = this.oldMin;
-    } else {
-      this.oldMin = this.min;
-      this.min = Number.MIN_SAFE_INTEGER;
-    }
-    // We should probably disable the min input too, and change text to be infinity symbol or something?
-  }
-
-  toggleMaxCut(change: MatCheckboxChange) {
-    const value = change.checked;
-    if (value) {
-      this.max = this.oldMax;
-    } else {
-      this.oldMax = this.max;
-      this.max = Number.MAX_SAFE_INTEGER;
-    }
-    // We should probably disable the min input too, and change text to be infinity symbol or something?
   }
 }
