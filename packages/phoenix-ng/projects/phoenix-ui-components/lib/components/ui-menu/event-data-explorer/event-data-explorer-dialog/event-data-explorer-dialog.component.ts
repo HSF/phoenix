c1fa47f0b59bd3d7907c2e163e39c0dec004766e
Sebastien Ponce <sebastien.ponce@cern.ch>
2023-06-27 12:55:39 +0200

    Moved file loading code from the ui component event-data-explorer-dialog to the dedicated service file-loader, for easier reuse

--- packages/phoenix-ng/projects/phoenix-ui-components/lib/components/ui-menu/event-data-explorer/event-data-explorer-dialog/event-data-explorer-dialog.component.ts
+++ packages/phoenix-ng/projects/phoenix-ui-components/lib/components/ui-menu/event-data-explorer/event-data-explorer-dialog/event-data-explorer-dialog.component.ts
@@ -1,10 +1,9 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { JiveXMLLoader } from 'phoenix-event-display';
 import { EventDisplayService } from '../../../../services/event-display.service';
 import { FileNode } from '../../../file-explorer/file-explorer.component';
+import { FileLoaderService } from '../../../../services/file-loader.service';
 import { EventDataExplorerDialogData } from '../event-data-explorer.component';
-import JSZip from 'jszip';
 
 const supportFileTypes = ['json', 'xml'];
 
@@ -26,20 +25,25 @@ export class EventDataExplorerDialogComponent {
 
   constructor(
     private eventDisplay: EventDisplayService,
+    private fileLoader: FileLoaderService,
     private dialogRef: MatDialogRef<EventDataExplorerDialogComponent>,
     @Inject(MAT_DIALOG_DATA) private dialogData: EventDataExplorerDialogData
   ) {
     // Event data
-    this.makeRequest(this.dialogData.apiURL, 'json', (res: FileResponse[]) => {
-      const filePaths = res.filter((file) =>
-        supportFileTypes.includes(file.name.split('.').pop())
-      );
+    fileLoader.makeRequest(
+      this.dialogData.apiURL,
+      'json',
+      (res: FileResponse[]) => {
+        const filePaths = res.filter((file) =>
+          supportFileTypes.includes(file.name.split('.').pop())
+        );
 
-      this.eventDataFileNode = this.buildFileNode(filePaths);
-    });
+        this.eventDataFileNode = this.buildFileNode(filePaths);
+      }
+    );
 
     // Config
-    this.makeRequest(
+    fileLoader.makeRequest(
       `${this.dialogData.apiURL}?type=config`,
       'json',
       (res: FileResponse[]) => {
@@ -53,96 +57,29 @@ export class EventDataExplorerDialogComponent {
   }
 
   loadEvent(file: string) {
-    const isZip = file.split('.').pop() === 'zip';
-    const rawfile = isZip ? file.substring(0, file.length - 4) : file;
-    this.makeRequest(file, isZip ? 'blob' : 'text', (eventData) => {
-      switch (rawfile.split('.').pop()) {
-        case 'xml':
-          this.loadJiveXMLEvent(eventData);
-          break;
-        case 'json':
-          this.loadJSONEvent(eventData);
-          break;
-      }
-    });
-  }
-
-  private loadJSONEvent(eventData: string) {
-    this.eventDisplay.parsePhoenixEvents(JSON.parse(eventData));
-    this.onClose();
-  }
-
-  private loadJiveXMLEvent(eventData: string) {
-    const jiveXMLLoader = new JiveXMLLoader();
-    jiveXMLLoader.process(eventData);
-    const processedEventData = jiveXMLLoader.getEventData();
-    this.eventDisplay.buildEventDataFromJSON(processedEventData);
-    this.onClose();
+    this.loading = true;
+    this.error = this.fileLoader.loadEvent(file, this.eventDisplay);
+    this.loading = false;
+    if (!this.error) this.onClose();
   }
 
   loadConfig(file: string) {
-    this.makeRequest(
+    this.loading = true;
+    this.error = this.fileLoader.makeRequest(
       `${this.dialogData.apiURL}?type=config&f=${file}`,
       'text',
       (config) => {
         const stateManager = this.eventDisplay.getStateManager();
         stateManager.loadStateFromJSON(JSON.parse(config));
-
         this.onClose();
       }
     );
+    this.loading = false;
   }
 
   // Helpers
-
   onClose() {
     this.dialogRef.close();
-  }
-
-  private async unzip(data: ArrayBuffer) {
-    const archive = new JSZip();
-    await archive.loadAsync(data);
-    let fileData = '';
-    let multiFile = false;
-    for (const filePath in archive.files) {
-      if (multiFile) {
-        console.error(
-          'Zip archive contains more than one file. Ignoring all but first'
-        );
-        break;
-      }
-      fileData = await archive.file(filePath).async('string');
-      multiFile = true;
-    }
-    return fileData;
-  }
-
-  makeRequest(
-    urlPath: string,
-    responseType: 'json' | 'text' | 'blob',
-    onData: (data: any) => void
-  ) {
-    this.loading = true;
-    fetch(urlPath)
-      .then((res) => res[responseType]())
-      .then((data) => {
-        if (responseType === 'blob') {
-          data
-            .arrayBuffer()
-            .then((buf) => this.unzip(buf))
-            .then((d) => onData(d));
-        } else {
-          onData(data);
-        }
-        this.error = false;
-      })
-      .catch((error) => {
-        console.error(error);
-        this.error = true;
-      })
-      .finally(() => {
-        this.loading = false;
-      });
   }
 
   private buildFileNode(filePaths: FileResponse[]): FileNode {
---
+++ packages/phoenix-ng/projects/phoenix-ui-components/lib/services/file-loader.service.ts
@@ -0,0 +1,90 @@
+import { Injectable } from '@angular/core';
+import JSZip from 'jszip';
+import { EventDisplayService } from './event-display.service';
+import { JiveXMLLoader } from 'phoenix-event-display';
+
+/**
+ * Service makeing file loading easier
+ */
+@Injectable({
+  providedIn: 'root',
+})
+export class FileLoaderService {
+  private lastEventsURL: string = '';
+
+  async unzip(data: ArrayBuffer) {
+    const archive = new JSZip();
+    await archive.loadAsync(data);
+    let fileData = '';
+    let multiFile = false;
+    for (const filePath in archive.files) {
+      if (multiFile) {
+        console.error(
+          'Zip archive contains more than one file. Ignoring all but first'
+        );
+        break;
+      }
+      fileData = await archive.file(filePath).async('string');
+      multiFile = true;
+    }
+    return fileData;
+  }
+
+  // returns whether an error was found
+  makeRequest(
+    urlPath: string,
+    responseType: 'json' | 'text' | 'blob',
+    onData: (data: any) => void
+  ) {
+    fetch(urlPath)
+      .then((res) => res[responseType]())
+      .then((data) => {
+        if (responseType === 'blob') {
+          data
+            .arrayBuffer()
+            .then((buf) => this.unzip(buf))
+            .then((d) => onData(d));
+        } else {
+          onData(data);
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        return true;
+      });
+    return false;
+  }
+
+  loadJSONEvent(eventData: string, eventDisplay: EventDisplayService) {
+    eventDisplay.parsePhoenixEvents(JSON.parse(eventData));
+  }
+
+  loadJiveXMLEvent(eventData: string, eventDisplay: EventDisplayService) {
+    const jiveXMLLoader = new JiveXMLLoader();
+    jiveXMLLoader.process(eventData);
+    const processedEventData = jiveXMLLoader.getEventData();
+    eventDisplay.buildEventDataFromJSON(processedEventData);
+  }
+
+  loadEvent(file: string, eventDisplay: EventDisplayService) {
+    this.lastEventsURL = file;
+    const isZip = file.split('.').pop() === 'zip';
+    const rawfile = isZip ? file.substring(0, file.length - 4) : file;
+    return this.makeRequest(file, isZip ? 'blob' : 'text', (eventData) => {
+      switch (rawfile.split('.').pop()) {
+        case 'xml':
+          this.loadJiveXMLEvent(eventData, eventDisplay);
+          break;
+        case 'json':
+          this.loadJSONEvent(eventData, eventDisplay);
+          break;
+      }
+    });
+  }
+
+  reloadLastEvents(eventDisplay: EventDisplayService) {
+    if (this.lastEventsURL.length > 0) {
+      this.loadEvent(this.lastEventsURL, eventDisplay);
+    }
+  }
+}
