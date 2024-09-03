881fca9076ea8aa7f15ba9b0ff1379a1e32d6fed
Edward Moyse <edward.moyse@gmail.com>
2024-09-03 16:50:46 +0200

    Adding eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,48 @@
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import prettier from "eslint-plugin-prettier";
+import tsParser from "@typescript-eslint/parser";
+import { includeIgnoreFile } from "@eslint/compat";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+const gitignorePath = path.resolve(__dirname, ".gitignore");
+
+
+export default [
+    includeIgnoreFile(gitignorePath),
+    ...compat.extends(
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "prettier",
+), {
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+        prettier,
+    },
+    languageOptions: {
+        parser: tsParser,
+    },
+
+    rules: {
+        "prettier/prettier": ["error", {
+            endOfLine: "auto",
+        }],
+
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-unused-vars": "off",
+        "@typescript-eslint/no-inferrable-types": "off",
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+        "@typescript-eslint/no-empty-function": "off",
+        "@typescript-eslint/no-this-alias": "off",
+    },
+}];
