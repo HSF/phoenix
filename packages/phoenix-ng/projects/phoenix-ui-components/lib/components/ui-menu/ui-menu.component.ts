09efeb8d6c4461eaeafc06176f5f070f6ce0355b
Devesh Bervar <dvsshbrvr@gmail.com>
2026-03-04 12:53:22 +0530

    feat: make UI components configurable via UIMenuConfig (#61)

--- packages/phoenix-ng/projects/phoenix-ui-components/lib/components/ui-menu/ui-menu.component.ts
+++ packages/phoenix-ng/projects/phoenix-ui-components/lib/components/ui-menu/ui-menu.component.ts
@@ -5,8 +5,40 @@ import {
 } from '../../services/extras/event-data-import';
 import { defaultAnimationPresets } from './animate-camera/animate-camera.component';
 
+export interface UIMenuConfig {
+  showVRToggle?: boolean;
+  showARToggle?: boolean;
+  showShareLink?: boolean;
+  showPerformanceToggle?: boolean;
+  showGeometryBrowser?: boolean;
+  showObjectClipping?: boolean;
+  showOverlayView?: boolean;
+  showAnimateEvent?: boolean;
+  showAnimateCamera?: boolean;
+  showCollectionsInfo?: boolean;
+  showMakePicture?: boolean;
+  showObjectSelection?: boolean;
+  showInfoPanel?: boolean;
+}
+
+export const defaultUIMenuConfig: UIMenuConfig = {
+  showVRToggle: true,
+  showARToggle: true,
+  showShareLink: true,
+  showPerformanceToggle: true,
+  showGeometryBrowser: true,
+  showObjectClipping: true,
+  showOverlayView: true,
+  showAnimateEvent: true,
+  showAnimateCamera: true,
+  showCollectionsInfo: true,
+  showMakePicture: true,
+  showObjectSelection: true,
+  showInfoPanel: true,
+};
+
 @Component({
-  standalone: false, // this is now required when using NgModule
+  standalone: false,
   selector: 'app-ui-menu',
   templateUrl: './ui-menu.component.html',
   styleUrls: ['./ui-menu.component.scss'],
@@ -15,6 +47,10 @@ export class UiMenuComponent {
   @Input()
   eventDataImportOptions: EventDataImportOption[] =
     Object.values(EventDataFormat);
+
   @Input()
   animationPresets = defaultAnimationPresets;
+
+  @Input()
+  uiConfig: UIMenuConfig = { ...defaultUIMenuConfig };
 }
